f2f7609330d4183b0f40cefb4f77940b0ea6ec38
shorinnn <shorinnn@yahoo.com>
2014-11-21 20:38:29 +0200

    Admin seeder, User model to use Ardent

--- laravel/app/database/seeds/DatabaseSeeder.php
+++ laravel/app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		 $this->call('RoleTableSeeder');
+		 $this->call('UserTableSeeder');
+		 $this->call('AssignedRoleTableSeeder');
 	}
 
 }
@@ -20,6 +22,7 @@ class RoleTableSeeder extends Seeder {
 
     public function run()
     {
+        DB::table('roles')->delete();
         $admin = new Role;
         $admin->name = 'Admin';
         $admin->save();
@@ -35,6 +38,42 @@ class RoleTableSeeder extends Seeder {
         $affiliate = new Role;
         $affiliate->name = 'Affiliate';
         $affiliate->save();
+    }
+
+}
+
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        $user = new User;
+        $user->username = 'superadmin';
+        $user->email = 'superadmin@wazaar.jp';
+        $user->password = 'BSVdwS8y3Xvv';
+        $user->password_confirmation = 'BSVdwS8y3Xvv';
+        $user->confirmation_code = md5(uniqid(mt_rand(), true));
+        $user->confirmed = 1;
+        if(! $user->save()) {
+          Log::info('Unable to create user '.$user->username, (array)$user->errors());
+        } else {
+          Log::info('Created user "'.$user->username.'" <'.$user->email.'>');
+        }
+        
+        
+
+    }
+}
+    
+class AssignedRoleTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('assigned_roles')->delete();
+        $user = User::where('username', '=', 'superadmin')->first();
+        $adminRole = Role::where('name','=','Admin')->first();
+        $user->attachRole( $adminRole );
+
     }
 
 }
--- laravel/app/models/User.php
+++ laravel/app/models/User.php
@@ -1,10 +1,17 @@
 <?php
-
+use LaravelBook\Ardent\Ardent;
 use Zizaco\Confide\ConfideUser;
 use Zizaco\Confide\ConfideUserInterface;
+use Zizaco\Entrust\HasRole;
 
 class User extends Ardent implements ConfideUserInterface
 {
-    use ConfideUser;
-    use HasRole; // Add this trait to your user model
+    use ConfideUser{
+        save as confideSave;
+    }
+    use HasRole;
+    
+    public function save(array $rules = Array(), array $customMessages = Array(), array $options = Array(), Closure $beforeSave = NULL, Closure $afterSave = NULL){
+        $this->confideSave();
+    }
 }
