261c99676a813e3fe6426d74faadffc7661a19e5
shorinnn <shorinnn@yahoo.com>
2014-11-21 20:53:11 +0200

    New ACL links

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -18,13 +18,13 @@ Route::get('/', function()
 //
 
 // Confide routes
-Route::get('users/create', 'UsersController@create');
+Route::get('register', 'UsersController@create');
 Route::post('users', 'UsersController@store');
-Route::get('users/login', 'UsersController@login');
-Route::post('users/login', 'UsersController@doLogin');
-Route::get('users/confirm/{code}', 'UsersController@confirm');
-Route::get('users/forgot_password', 'UsersController@forgotPassword');
-Route::post('users/forgot_password', 'UsersController@doForgotPassword');
-Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
-Route::post('users/reset_password', 'UsersController@doResetPassword');
-Route::get('users/logout', 'UsersController@logout');
+Route::get('login', 'UsersController@login');
+Route::post('login', 'UsersController@doLogin');
+Route::get('account_confirmation/{code}', 'UsersController@confirm');
+Route::get('forgot_password', 'UsersController@forgotPassword');
+Route::post('forgot_password', 'UsersController@doForgotPassword');
+Route::get('reset_password/{token}', 'UsersController@resetPassword');
+Route::post('reset_password', 'UsersController@doResetPassword');
+Route::get('logout', 'UsersController@logout');
