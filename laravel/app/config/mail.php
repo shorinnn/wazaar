5f3582ca893fd343c907cc9603454b16c5849891
shorinnn <shorinnn@yahoo.com>
2014-11-21 19:09:11 +0200

    Zizaco\Confide install, custom users table fields

--- laravel/app/config/mail.php
+++ laravel/app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'hello@wazaar.dev', 'name' => 'wazaar team'),
 
 	/*
 	|--------------------------------------------------------------------------
