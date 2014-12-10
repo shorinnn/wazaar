ba1d146b906b162ff7c260e1e50512db643e5429
Macclinson <macclinson@yahoo.com>
2014-12-10 16:26:29 +0100

    Register and Login page

--- laravel/app/views/confide/login.blade.php
+++ laravel/app/views/confide/login.blade.php
@@ -1,5 +1,22 @@
     @extends('layouts.default')
-    @section('content')	
+    @section('content')
+    <section class="container-fluid login-page top-blue-section">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Login</h1>
+            </div>
+        </div>
+	</section>
+    <div class="container login-page">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="#" class="fb-button social-button">Sign in with Facebook</a>
+                <a href="#" class="google-button social-button">Sign in with Google</a>
+                <p class="or">Or</p>
+            </div>
+        </div>
+    </div>
+
     <form id='login-form' role="form" method="POST" action="{{{ action('UsersController@login') }}}" accept-charset="UTF-8">
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
     <fieldset>
@@ -12,11 +29,11 @@
             {{{ Lang::get('confide::confide.password') }}}
         </label>
         <input class="form-control" tabindex="2" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
-        <p class="help-block">
+        <p class="help-block forgot-password">
             <a href="{{{ action('UsersController@forgotPassword') }}}">{{{ Lang::get('confide::confide.login.forgot_password') }}}</a>
         </p>
         </div>
-        <div class="checkbox">
+        <div class="checkbox remember-me">
             <label for="remember">
                 <input type="hidden" name="remember" value="0">
                 <input tabindex="4" type="checkbox" name="remember" id="remember" value="1"> {{{ Lang::get('confide::confide.login.remember') }}}
@@ -29,7 +46,7 @@
         @if (Session::get('notice'))
             <div class="alert">{{{ Session::get('notice') }}}</div>
         @endif
-        <div class="form-group">
+        <div class="form-group login-button">
             <button tabindex="3" type="submit" class="btn btn-default">{{{ Lang::get('confide::confide.login.submit') }}}</button>
         </div>
     </fieldset>
--- laravel/app/views/confide/signup.blade.php
+++ laravel/app/views/confide/signup.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.default')
 @section('content')
-
+<section class="container-fluid register-page top-blue-section">
+	<div class="row">
+    	<div class="col-md-12">
+        	<h1>Register</h1>
+            <span>and earn</span>
+            <em>500¥</em>
+            <p>credit</p>
+        </div>
+    </div>
+</section>
 @if (Session::get('error'))
     <div class="alert alert-error alert-danger">
         @if (is_array(Session::get('error')))
@@ -12,6 +21,15 @@
 @if (Session::get('notice'))
     <div class="alert">{{ Session::get('notice') }}</div>
 @endif
+<div class="container register-page">
+	<div class="row">
+    	<div class="col-md-12">
+        	<a href="#" class="fb-button social-button">Sign up with Facebook</a>
+            <a href="#" class="google-button social-button">Sign up with Google</a>
+            <p class="or">Or</p>
+        </div>
+    </div>
+</div>
 <form method="POST" action="{{{ URL::to('users') }}}" accept-charset="UTF-8" id="register-form">
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
     <fieldset>
@@ -19,6 +37,7 @@
             <label for="email">{{{ Lang::get('confide::confide.e_mail') }}} <small>{{ Lang::get('confide::confide.signup.confirmation_required') }}</small></label>
             <input class="form-control" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
         </div>
+        <p class="clearfix"><em class="left">Almost there!</em> <span class="block right">8 to 20 characters</span></p>
         <div class="form-group">
             <label for="password">{{{ Lang::get('confide::confide.password') }}}</label>
             <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
