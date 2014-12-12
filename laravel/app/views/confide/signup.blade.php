4e3f66442daa83ea0276d171d56edbda194ad89e
shorinnn <shorinnn@yahoo.com>
2014-12-12 23:38:21 +0200

    Implemented parsley for registration form

--- laravel/app/views/confide/signup.blade.php
+++ laravel/app/views/confide/signup.blade.php
@@ -23,12 +23,14 @@
         </div>
     </div>
 </div>
-<form method="POST" action="{{{ URL::to('users') }}}" accept-charset="UTF-8" id="register-form">
+<form method="POST" action="{{{ URL::to('users') }}}" accept-charset="UTF-8" id="register-form" data-parsley-validate>
     <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
     <fieldset>
         <div class="form-group email-input-container">
             <label for="email">{{{ Lang::get('confide::confide.e_mail') }}} <small>{{ Lang::get('confide::confide.signup.confirmation_required') }}</small></label>
-            <input class="form-control active-input" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+            <input class="form-control active-input" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" 
+                   type="email" name="email" id="email" value="{{{ Input::old('email') }}}" 
+                   required  data-parsley-trigger="change" />
 	        <p class="clearfix character-tip"><em class="left"></em><span class="block right"></span></p>
         </div>
         <p class="js-error-message"></p>
@@ -42,12 +44,16 @@
         @endif
         <div class="form-group">
             <label for="password">{{{ Lang::get('confide::confide.password') }}}</label>
-            <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" type="password" name="password" id="password">
+            <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password') }}}" 
+                   type="password" name="password" id="password" 
+                   required  data-parsley-trigger="change" data-parsley-minlength="6" />
 	        <p class="clearfix character-tip"><span class="block">At least 6 characters</span></p>
         </div>
         <div class="form-group">
             <label for="password_confirmation">{{{ Lang::get('confide::confide.password_confirmation') }}}</label>
-            <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password_confirmation') }}}" type="password" name="password_confirmation" id="password_confirmation">
+            <input class="form-control" placeholder="{{{ Lang::get('confide::confide.password_confirmation') }}}" 
+                   type="password" name="password_confirmation" id="password_confirmation" 
+                   data-parsley-equalto="#password" required  data-parsley-trigger="change" data-parsley-minlength="6" />
 	        <p class="clearfix character-tip"><span class="block">Type in the password again</span></p>
         </div>
         <input class="form-control" type="hidden" name="instructor" id="instructor" value="{{{ $instructor }}}">
--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -3,11 +3,26 @@ $(document).ready(function(){
     $(".profile-name > li").removeClass("activate-dropdown");
     // attach event to body, this allows the function to run when dynamically loaded (ajax) btns are clicked
     $('body').delegate('.delete-button', 'click', confirmDelete);
-    $('body').delegate('#register-form .form-group input', 'focus', slideDownText);
-    $('body').delegate('#register-form [name="email"]', 'blur', validateEmail);
-    $('body').delegate('#register-form [name="password"]', 'blur', validatePassword);
-    $('body').delegate('#register-form [name="password_confirmation"]', 'blur', confirmPassword);
+    // listen for validated fields
+    $.listen('parsley:field:success', field_validated);
 });
+
+function field_validated(e){
+    // get the parent form
+    $form = e.$element.closest('form');
+    // loop through elements and check if all valid
+    all_valid = true;
+    $form.find("[data-parsley-trigger]").each(
+        function(){
+            if($(this).hasClass() || $(this).attr('id')!=''){
+                if(!$(this).parsley().isValid()) all_valid = false;
+            }
+        }
+    );
+    if(all_valid){
+        alert('Doing some success animation here here - this is triggered by successfully filling out the entire form')
+    }
+}
 
 function confirmDelete(e){
     // get the message from the clicked button, don't hard code it (so we can use localization)
@@ -15,70 +30,12 @@ function confirmDelete(e){
     return confirm(msg);
 }
 
-function slideDownText(e){
-   $(e.target).focus(function(){
-       $(this).next().addClass('slide-down');
-   });
-   $(e.target).blur(function(){
-       $(this).next().removeClass('slide-down');
-    });
-    return;
-}
-
-function validateEmail(e){
-    // Get the email value
-    var email = $(e.target);
-    $(this).focus().addClass("active-input");
-    // Email validation Regex
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    //Compare Email with input value
-     if(email.val().match(re)){
-         $('#password').focus().addClass("active-input");
-         email.removeClass("active-input");
-         $(".js-error-message").css("display", "none");
-         return;
-     }
-
-    // Handle any error display message or pop up here
-    $(".js-error-message").append("<span>ERROR</span>Please enter a valid Email Address.").css("display", "block");
-     return false;
-}
-
-function validatePassword(e){
-    // Get the password
-    var password = $(e.target);
-    $(this).focus().addClass("active-input");
-    //Check length of password and ensure its not less than 6
-    if(password.val().length >= 6){
-        $('#password').removeClass("active-input");
-        $("#password_confirmation").focus().addClass("active-input");
-        $(".js-error-message").css("display", "none");
-        return;
-    }
-
-    //Handle any error message and retain focus on the input box
-    $(".js-error-message").html("");
-    $(".js-error-message").append("<span>ERROR</span>Enter a minimum of 6 characters.").css({"display":"block", "top":"72px"});
-    return;
-}
-function confirmPassword(e){
-    // Get the first password entered.
-    var firstPassword = $("#password");
-    $(this).focus().addClass("active-input");
-    //Get the second password entered
-    var secondPassword = $(e.target);
-
-    //Check that second password matches first password
-    if(secondPassword.val() == firstPassword.val()){
-        $("#password_confirmation").removeClass("active-input");
-        $("#register-form .btn.btn-primary").addClass("active-button");
-        $(".js-error-message").css("display", "none");
-        return;
-    }
-
-    //Display some error message.
-    $(".js-error-message").html("");
-    $(".js-error-message").append("<span>ERROR</span>Password does not match.").css({"display":"block", "top":"157px"});
-    return;
-}
+//function slideDownText(e){
+//   $(e.target).focus(function(){
+//       $(this).next().addClass('slide-down');
+//   });
+//   $(e.target).blur(function(){
+//       $(this).next().removeClass('slide-down');
+//    });
+//    return;
+//}
