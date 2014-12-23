bbb51f237adc825ad1c598daaea9007dd27512d0
Albert <albertmaranian@gmail.com>
2014-12-23 23:01:56 +0800

    Added a trait to get profile for each user type

--- laravel/app/models/Traits/ProfileTrait.php
+++ laravel/app/models/Traits/ProfileTrait.php
@@ -3,4 +3,9 @@
 
 trait ProfileTrait {
 
+    public function profile()
+    {
+        $obj = (new static);
+        return $this->hasOne('Profile', 'user_id')->where('role_id', $obj->roleId)->first();
+    }
 }
