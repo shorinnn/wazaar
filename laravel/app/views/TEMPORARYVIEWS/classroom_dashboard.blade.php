f30e0f8cdbafb9ed504ff9a02bacff429854d1a7
Macclinson <macclinson@yahoo.com>
2015-01-14 05:57:50 +0100

    Unauthenticated Homepage

--- laravel/app/views/TEMPORARYVIEWS/classroom_dashboard.blade.php
+++ laravel/app/views/TEMPORARYVIEWS/classroom_dashboard.blade.php
@@ -1,4 +1,24 @@
     @extends('layouts.default')
     @section('content')	
-        Classroom DASHBOARD
+    <div class="wrapper classroom-dashboard">
+    	<div class="container classroom-stats">
+        	<div class="row">
+            	<div class="col-md-6 col-sm-6 col-xs-12">
+                	<div class="today-stat">
+                    	<h2>Today
+                        	<span>43</span>
+                        </h2>
+                        <h4>NEW STUDENTS</h4>
+                        <ul>
+                        	<li id="monday">
+                            	<span>Monday</span>
+                                <div><span></span></div>
+                                <em>5</em>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     @stop
--- laravel/app/views/site/homepage_unauthenticated.blade.php
+++ laravel/app/views/site/homepage_unauthenticated.blade.php
@@ -1,4 +1,57 @@
     @extends('layouts.default')
     @section('content')	
-    Page for unauthenticated users
+        <section class="container-fluid course-search-section unauthenticated-homepage">
+        	<div class="container">
+            	<div class="row">
+                	<div class="col-md-12">
+                    	<a href="#" class="logo">
+                        	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/logo/logged-out-home-logo.png" alt="">
+                        </a>
+                        <div class="color-bubbles">
+                        	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/icons/color-bubbles.png" alt="" class="img-responsive">
+                        </div>
+                    	<p class="lead">Advance <span>Your Career.</span> Discover new skills. 
+                        </p>
+                        <p class="lead">
+                        Learn what you never got a chance to learn at school.                        
+                        </p>
+                        <div class="course-search-form">
+                        	<form>
+                            	<input type="search" placeholder="E.g. Javascript, online business, etc ..." name="course-search">
+                                <button></button>
+                            </form>
+                            <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/sample-search-category.png" 
+                            alt="" class="img-responsive">
+                        </div>
+                        <a href="#" class="discover-wazaar-button">
+                        Discover Wazaar
+                        </a>
+                        <span class="down-arrow">
+                        	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/icons/down-arrow.png">
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="unauthenticated-homepage">
+        	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/home-stock-images/home-stock-image5.jpg" class="img-responsive">
+        </section>
+        <section class="school-is-a-thing unauthenticated-homepage clearfix">
+        	<div>
+            	<h3>School is a thing of yesterday.
+                	<span>This is the new way to upgrade yourself. Save time and money, and get a promotion.</span>
+                </h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo. Lorem ipsum dolor sit amet, 
+                consectetur adipisicing elit, sed do 
+                </p>
+                <a href="#" class="sign-up-button">Sign up and earn 500¥ credit</a>
+            </div>
+            <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/home-stock-images/home-stock-image6.jpg" alt="" 
+            class="hidden-xs img-responsive">
+        </section>
+        <section class="unauthenticated-homepage">
+        	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/home-stock-images/home-stock-image7.jpg" class="img-responsive">
+        </section>
     @stop
