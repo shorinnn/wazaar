af8b1602433f2ece366930440052c3bd32ba081a
shorinnn <shorinnn@yahoo.com>
2015-01-23 12:33:38 +0200

    joke shop addition

--- laravel/app/views/TEMPORARYVIEWS/shop.blade.php
+++ laravel/app/views/TEMPORARYVIEWS/shop.blade.php
@@ -11,6 +11,12 @@
                     accurate and comprehensive. In order to maintain a smooth shopping experience, it is important t
                     hat a user sees the same information about products
                     <br />
+                    <center>
+                        <h2>The Cocorium revolution will change the world</h2<br />
+                    <iframe src="http://free.timeanddate.com/countdown/i4ijj3n2/n770/cf110/cm0/cu4/ct0/cs0/ca0/co0/cr0/ss0/cac000/cpc000/pca0bfd5/tcfff/fs100/szw448/szh189/iso2015-04-01T00:00:00/bo2" frameborder="0" width="450" height="191"></iframe>
+                    </center>
+                    <br />
+                    <a href="#testimonials"><h1>See what our customers say:</h1></a>
                     <br />
                     <select>
                         <option>Filter</option>
@@ -28,6 +34,16 @@
                     <br />
                     <div class="row">
                         <div class="col-lg-4">
+                            <img src="http://shorinnn.com/easteregg/NewIcon.png" style="float:left; height:70px; 
+                                 position:absolute; margin-left:-20px; margin-top:-20px" />
+                            <a href="http://shorinnn.com/easteregg/okay.png" target="_blank">
+                                <img width="200" src="http://shorinnn.com/easteregg/okay.png" />
+                            </a>
+                            <p style='font-style:italic'>Need I say more?</p>
+                            <p>$350K (out of stock)</p>
+                            <button class="btn btn-danger"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
+                        </div>
+                        <div class="col-lg-4">
                             <a href="http://shorinnn.com/easteregg/1.png" target="_blank">
                                 <img width="200" src="http://shorinnn.com/easteregg/1.png" />
                             </a><br />
@@ -43,6 +59,10 @@
                             <p>$50.000.49 (out of stock)</p>
                             <button class="btn btn-danger"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
                         </div>
+     
+                    </div>
+                    
+                    <div class="row">
                         <div class="col-lg-4">
                             <a href="http://shorinnn.com/easteregg/3.png" target="_blank">
                                 <img width="200" src="http://shorinnn.com/easteregg/3.png" />
@@ -51,10 +71,6 @@
                             <p>$23.235.01 (out of stock)</p>
                             <button class="btn btn-danger"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
                         </div>
-     
-                    </div>
-                    
-                    <div class="row">
                         <div class="col-lg-4">
                             <a href="http://shorinnn.com/easteregg/4.png" target="_blank">
                                 <img width="200" src="http://shorinnn.com/easteregg/4.png" />
@@ -72,11 +88,61 @@
                             <p>$200K (1 left - buy now!)</p>
                             <button class="btn btn-danger"><i class="fa fa-shopping-cart"></i> Add To Cart</button>
                         </div>
-                        <div class="col-lg-4">
-                           
-                        </div>
+       
      
                     </div>
+                    
+                    <a name="testimonials"></a>
+                    <section class="main-content-container">
+                        <div class="main-content">
+                        <h1>Testimonials</h1>
+                            <div class="testimonials col-lg-6">
+                                <p>
+                                Switched from MaxCDN and never looked back.
+                                </p>
+                                <span class="name">Ryan Ehr</span>
+                            </div>
+                            <div class="testimonials col-lg-6">
+                                <p>
+                                Really good. No matter where you go, Cocorium is the coolest, most happening thing around! Definitely worth the investment.
+                                </p>
+                                <span class="name">Coralyn T</span>
+                            </div>
+                        <br class="clear" />
+                        <hr class="clear" />
+                        <br class="clear" />
+                            <div class="testimonials  col-lg-6">
+                                <p>
+                                I would also like to say thank you to all your staff. It really saves me time and effort. Cocorium is exactly what our business has been lacking.
+                                </p>
+                                <span class="name">Melisandra F</span>
+                            </div>
+  
+                            <div class="testimonials  col-lg-6">
+                                <p>
+                                Cocorium was the best investment I ever made. Best. Product. Ever!
+                                </p>
+                                <span class="name"> Mayda I.</span>
+                            </div>
+                      <br class="clear" />
+                      <hr class="clear" />
+                      <br class="clear" />
+                            <div class="testimonials col-lg-6">
+                                <p>
+                                 We have no regrets! It's incredible.
+                                </p>
+                                <span class="name">Brandi N.</span>
+                                <br class="clear" />
+                            </div>
+                            <div class="testimonials col-lg-6">
+                                <p>
+                                 Thanks Cocorium! It fits our needs perfectly. I love Cocorium.
+                                </p>
+                                <span class="name"> Martica N.</span>
+                                <br class="clear" />
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
