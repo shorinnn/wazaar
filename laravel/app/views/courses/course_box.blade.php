ba09979ee315e92fe55713f1ef9edc4cf4f218ff
Macclinson <macclinson@yahoo.com>
2015-01-27 09:33:20 +0100

    New Course Listing

--- laravel/app/views/courses/course_box.blade.php
+++ laravel/app/views/courses/course_box.blade.php
@@ -1,15 +1,10 @@
 <div class="col-xs-12 col-sm-6 col-md-4">
     <div class="object small-box small-box-one">
-        <div class="price-tag">
-             ¥ {{ number_format($course->cost(), Config::get('custom.currency_decimals')) }}
-            @if($course->isDiscounted())
-                {{trans('courses/general.sale')}}
-            @endif
-        </div>
-        <div class="level">{{ $course->courseDifficulty->name }}</div>
-        @if($course->isNew())
+        <!--<div class="level">{{ $course->courseDifficulty->name }}</div>-->
+        <!--@if($course->isNew())
             <div class="new-tag">NEW</div>
-        @endif
+        @endif-->
+        <div class="img-container">
         <img 
             @if($course->course_preview_image_id == null)
                 src="http://placehold.it/350x150&text=Preview Unavailable"
@@ -17,6 +12,8 @@
                 src="{{$course->previewImage->url}}"
             @endif
              class="img-responsive" alt="">
+             <div class="sale-ends">SALE ENDS IN 2 DAYS 15:22:21</div>
+        </div>
         <h2>{{ $course->name }}</h2>
         <p>{{ $course->description }}
             <br />
@@ -24,14 +21,21 @@
                 <a href="{{action('CoursesController@subCategory', [$course->courseCategory->slug, $course->courseSubcategory->slug] )}}">{{$course->courseSubcategory->name}}</a>
             </small>
         </p>
-        
+        <div class="price-tag clear">
+             ¥ {{ number_format($course->cost(), Config::get('custom.currency_decimals')) }}
+            @if($course->isDiscounted())
+                {{trans('courses/general.sale')}}
+            @endif
+        </div>
+
         <div class="next_">
-            <div class="learn-more">
+            <!--<div class="learn-more">
                 <a href="{{action("CoursesController@show", $course->slug)}}">Learn more</a>
-            </div>
+            </div>-->
             <div class="students-attending">
                 {{ $course->student_count }} {{Lang::choice('general.student', $course->student_count)}}
-            </div>            
+            </div>   
+            <span class="likes">89%</span>         
         </div> 
     </div>
 </div>
