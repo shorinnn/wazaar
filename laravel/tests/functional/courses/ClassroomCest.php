edc3004bea0b3e5ff80d40de5dd45894c0a556ad
shorinnn <shorinnn@yahoo.com>
2015-01-27 19:10:01 +0200

    update codeception tests

--- laravel/tests/functional/courses/ClassroomCest.php
+++ laravel/tests/functional/courses/ClassroomCest.php
@@ -5,45 +5,47 @@ class ClassroomCest{
     
     public function _before(FunctionalTester $I){
         $I->haveEnabledFilters();
-//        $student = Student::where('username','student')->first();
-//        $lesson = Lesson::first();
-//        $student->purchase( $lesson->module->course );
+        $student = Student::where('username','student')->first();
+        $lesson = Lesson::first();
+        $student->purchase( $lesson->module->course );
     }
 
-//    public function seeNoCourse(FunctionalTester $I){
-//        $user = Student::where('username','mac')->first();
-//        $I->amLoggedAs($user);
-//        $I->amOnPage('/student/mycourses');
-//        $I->dontSee('Go To Dashboard');
-//    }   
-//    
-//    public function seePurchasedCourse(FunctionalTester $I){
-//        $user = Student::where('username','mac')->first();
-//        $lesson = Lesson::first();
-//        $user->purchase( $lesson->module->course );
-//        $I->amLoggedAs($user);
-//        $I->amOnPage('/student/mycourses');
-//        $I->see('Go To Dashboard');
-//    }
-//    
-//    public function goToCourseDash(FunctionalTester $I){
-//        $user = Student::where('username','mac')->first();
-//        $lesson = Lesson::first();
-//        $user->purchase( $lesson->module->course );
-//        $I->amLoggedAs($user);
-//        $I->amOnPage('/student/mycourses');
-//        $I->click('Go To Dashboard');
-//        $I->seeCurrentUrlEquals("/classroom/".$lesson->module->course->slug."/dashboard");
-//    }
-//    
+    public function seeNoCourse(FunctionalTester $I){
+        $user = Student::where('username','mac')->first();
+        $I->amLoggedAs($user);
+        $I->amOnPage('/student/mycourses');
+        $I->dontSee('Go To Dashboard');
+    }   
+    
+    public function seePurchasedCourse(FunctionalTester $I){
+        $user = Student::where('username','mac')->first();
+        $lesson = Lesson::first();
+        $user->purchase( $lesson->module->course );
+        $I->amLoggedAs($user);
+        $I->amOnPage('/student/mycourses');
+        $I->see('Go To Dashboard');
+    }
+    
+    public function goToCourseDash(FunctionalTester $I){
+        $user = Student::where('username','mac')->first();
+        $lesson = Lesson::first();
+        $user->purchase( $lesson->module->course );
+        $I->amLoggedAs($user);
+        $I->amOnPage('/student/mycourses');
+        $I->click('Go To Dashboard');
+        $I->seeCurrentUrlEquals("/classroom/".$lesson->module->course->slug."/dashboard");
+    }
+    
     public function markLessonViewed(FunctionalTester $I){
         $user = Student::where('username','mac')->first();
         $lesson = Lesson::first();
         $user->purchase( $lesson->module->course );
         $I->assertFalse( $user->isLessonViewed($lesson) );
         $I->amLoggedAs($user);
-        $I->amOnPage( action('ClassroomController@lesson', [$lesson->module->course->slug, $lesson->slug] ) );    
+        $I->dontSeeRecord('viewed_lessons', ['student_id' => $user->id, 'lesson_id' => $lesson->id]);
+        $I->amOnPage( action('ClassroomController@lesson', ['course'=>$lesson->module->course->slug, 'lesson'=>$lesson->slug] ) );    
         $user = Student::where('username','mac')->first();
+        $I->seeRecord('viewed_lessons', ['student_id' => $user->id, 'lesson_id' => $lesson->id]);
         $I->assertTrue( $user->isLessonViewed($lesson) );
     }
     
