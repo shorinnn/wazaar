9c4abaf4efcaa7a19b60fa9397a84c57ac8f1941
Macclinson <macclinson@yahoo.com>
2015-02-12 15:47:34 +0100

    Curriculum Lessons/Module Animation

--- laravel/app/views/layouts/default.blade.php
+++ laravel/app/views/layouts/default.blade.php
@@ -54,7 +54,7 @@
         </footer>
     </div>    
     @if( getenv('USE_COMMENTABLE_RESOURCES')==true )
-            <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         <script src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/js/parsley.min.js"></script>-->
         <script src="{{url("js/jquery.min.js")}}"></script>
