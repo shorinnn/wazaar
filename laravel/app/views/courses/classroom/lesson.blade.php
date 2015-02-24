e3a8112eb902c89da4e2e04756dddf19a3b49e8d
Macclinson <macclinson@yahoo.com>
2015-02-24 09:39:51 +0100

    Classroom and video skinning

--- laravel/app/views/courses/classroom/lesson.blade.php
+++ laravel/app/views/courses/classroom/lesson.blade.php
@@ -1,101 +1,116 @@
 @extends('layouts.default')
-
 @section('content')
-<style>
-    .replies:not(:empty){
-        border:1px solid silver;
-        width:80%;
-        min-width: 600px;
-    }
     
-    .comment-form-reply{
-        min-width: 500px
-    }
-    .is-reply-to{
-        font-style: italic;
-        font-size: 12px;
-    }
-    
-    .fa-animated{
-        -webkit-transition: all 0.3s; /* For Safari 3.1 to 6.0 */
-        transition: all 0.3s;
-    }
-    
-    .reply-to-label{
-        border: 1px solid silver;
-        border-radius: 5px;
-        padding: 2px;
-        background-color: #19B5DA;
-        color: white;
-        position: absolute;
-        z-index: 10;
-        margin-left: 90px;
-        margin-top: 15px;
-    }
-    a.load-remote > *{
-        pointer-events: none;
-    }
-    
-    .overlay-loading{
-        position:absolute;
-        margin-left:auto;
-        margin-right:auto;
-        left:0;
-        right:0;
-        z-index: 10;
-        width:32px;
-        height:32px;
-        background-image:url('http://www.mytreedb.com/uploads/mytreedb/loader/ajax_loader_blue_32.gif');
-    }
-</style>
-    <h1>Course: {{ $course->name }}</h1>
-    <h2>Lesson: {{ $lesson->name }}</h2>
-    
-    @if( $video != null)
-        <div class="text-center" style="border:1px solid gray; padding: 30px; margin:30px; background-color:silver">
-            @if( Agent::isMobile() )
-                <video controls><source src="{{ $video->video()->formats()->where('resolution', 'Custom Preset for Mobile Devices')
-                            ->first()->video_url }}" type="video/mp4"></video>
-            @else
-            <video controls><source src="{{ $video->video()->formats()->where('resolution', 'Custom Preset for Desktop Devices')
-                            ->first()->video_url }}" type="video/mp4"></video>
+        <div class="classrooms-wrapper clearfix">
+            <h1 class="classroom-course-title">Course: {{ $course->name }}</h1>
+            <h2 class="classroom-lesson-title">Lesson: {{ $lesson->name }}</h2>
+        	<section class="video-container">
+            	@if( $video != null)
+                <div class="text-center">
+                    @if( Agent::isMobile() )
+                        <video controls>
+                        	<source src="{{ $video->video()->formats()->where('resolution', 'Custom Preset for Mobile Devices')
+                            ->first()->video_url }}" type="video/mp4">
+                        </video>
+                    @else
+                    <div class="videoContainer">
+                        <video id="myVideo" preload="auto">
+                            <source src="{{ $video->video()->formats()->where('resolution', 'Custom Preset for Desktop Devices')
+                            ->first()->video_url }}" type="video/mp4">
+                        	<p>Your browser does not support the video tag.</p>
+                        </video>                        
+                        <div class="control">
+                    
+                            <div class="topControl">
+                                <div class="progress">
+                                    <span class="bufferBar"></span>
+                                    <span class="timeBar"></span>
+                                </div>
+                            </div>
+                            
+                            <div class="btmControl clearfix">
+                                <div class="btnPlay btn" title="Play/Pause video"></div>
+                                <div class="sound sound2 btn" title="Mute/Unmute sound"></div>
+                                <div class="volume-container">
+                                    <div class="volume" title="Set volume">
+                                        <span class="volumeBar">
+                                        	<em></em>
+                                        </span>
+                                    </div>
+                                </div>
+                                <div class="btnFS btn" title="Switch to full screen"></div>
+                                <div class="time">
+                                    <span class="current"></span> / 
+                                    <span class="duration"></span> 
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="loading"></div>
+                    </div>
+                    <span class="centered-play-button"></span>
+                    @endif
+                </div>
+            	@endif
+            </section>
+            <section class="classroom-content container">
+            	<div class="row first-row">
+                	<div class="col-md-12">
+                      @foreach($lesson->blocks as $block)
+                          @if($block->type=='text')
+                              <div class="well">{{ $block->content }}</div>
+                          @endif
+                      @endforeach
+                    </div>
+                </div>
+                <div class="row second-row">
+                	<div class="col-md-12">
+                    	<div class="files">
+                            @foreach($lesson->blocks as $block)
+                                @if($block->type=='file')
+                                    <p><a href='{{ $block->content }}' target='_blank'><i class="fa fa-cloud-download"></i> {{ $block->name }}</a></p>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="row classmate-conversations-heading">
+                	<div class="col-md-12">
+                        <p class="lead">Conversations:</p>
+                    </div>
+                </div>
+            </section>
+            <section class="classroom-content container">
+            @if( Auth::check() )
+                {{ View::make('courses.classroom.conversations.form')->with( compact('lesson') ) }}
             @endif
+            
+                <div class='ajax-content fa-animated'>
+                    {{ View::make('courses.classroom.conversations.all')->withComments( $lesson->comments ) }}
+        
+                   <!-- <a href='{{ action('ConversationsController@lesson', [$lesson->module->course->slug, $lesson->slug] )}}' class="load-more-comments load-more-ajax" 
+                       data-url='{{action('ConversationsController@loadMore')}}' 
+                       data-target='.users-comments' data-skip='2' data-post-field='lesson' data-id='{{$lesson->id}}'>LOAD MORE</a>
+                    -->
+                    <br />
+                    <div class="text-center load-remote" data-target='.ajax-content' data-load-method="fade">
+                        
+                        {{ $lesson->comments->links() }}
+                        <!--<a href='{{ action('ConversationsController@lesson', [$lesson->module->course->slug, $lesson->module->slug, $lesson->slug] )}}'>View All Comments Ever</a>-->
+                    </div>
+                </div>
+            </section>
+            <section class="container-fluid become-an-instructor">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h1>BECOME</h1>
+                      <h2>AN INSTRUCTOR</h2>
+                      <a href="{{ action('InstructorsController@become') }}"><span>{{trans('site/homepage.get-started')}}</span></a>
+                    </div>
+                  </div>
+              </div>
+            </section>
         </div>
-    @endif
-    
-    @foreach($lesson->blocks as $block)
-        @if($block->type=='text')
-            <div class="well">{{ $block->content }}</div>
-        @endif
-    @endforeach
-    
-    
-    @foreach($lesson->blocks as $block)
-        @if($block->type=='file')
-            <p><a href='{{ $block->content }}' target='_blank'><i class="fa fa-cloud-download"></i> {{ $block->name }}</a></p>
-        @endif
-    @endforeach
-   
-    <h2>Conversations:</h2>
-    <section class="classroom-content container">
-    @if( Auth::check() )
-        {{ View::make('courses.classroom.conversations.form')->with( compact('lesson') ) }}
-    @endif
-    
-        <div class='ajax-content fa-animated'>
-            {{ View::make('courses.classroom.conversations.all')->withComments( $lesson->comments ) }}
-
-           <!-- <a href='{{ action('ConversationsController@lesson', [$lesson->module->course->slug, $lesson->slug] )}}' class="load-more-comments load-more-ajax" 
-               data-url='{{action('ConversationsController@loadMore')}}' 
-               data-target='.users-comments' data-skip='2' data-post-field='lesson' data-id='{{$lesson->id}}'>LOAD MORE</a>
-            -->
-            <br />
-            <div class="text-center load-remote" data-target='.ajax-content' data-load-method="fade">
-                
-                {{ $lesson->comments->links() }}
-                <!--<a href='{{ action('ConversationsController@lesson', [$lesson->module->course->slug, $lesson->module->slug, $lesson->slug] )}}'>View All Comments Ever</a>-->
-            </div>
-        </div>
-    </div>
-    
 @stop
+
--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
     floatingNav();
     scrollNavigation();
 	fullScreen();
+	skinVideoControls();
 });
 
 /**
@@ -621,7 +622,7 @@ function fullScreen(){
 		  right: 0,
 		  bottom: 0,
 		  width: "100%", 
-		  height: "590px", 
+		  height: "590px"
 	  }
   
   //Properties to animate to
@@ -710,4 +711,225 @@ function fullScreen(){
   });
   
 }
- 
+
+function skinVideoControls(){
+	//INITIALIZE
+	var video = $('#myVideo');
+
+	//Set the width of the control to equal video width
+	if(video){
+        var playerWidth = video.innerWidth();
+		$('.control').width(playerWidth);
+	}
+	//remove default control when JS loaded
+	video[0].removeAttribute("controls");
+	$('.control').show();
+
+	//before everything get started
+	video.on('loadedmetadata', function() {
+		//set video properties
+		$('.current').text(timeFormat(0));
+
+		//start to get video buffering data 
+		setTimeout(startBuffer, 150);
+			
+	});
+	
+	//display current video play time
+	video.on('timeupdate', function() {
+		var currentPos = video[0].currentTime;
+		var maxduration = video[0].duration;
+		var perc = 100 * currentPos / maxduration;
+		$('.timeBar').css('width',perc+'%');	
+		$('.current').text(timeFormat(currentPos));	
+	});
+	
+	//CONTROLS EVENTS
+	//video screen and play button clicked
+	video.on('click', function() { playpause(); } );
+	$('.btnPlay, .centered-play-button').on('click', function() {
+        playpause();
+    });
+	var playpause = function() {
+		if(video[0].paused || video[0].ended) {
+			$('.btnPlay').addClass('paused');
+			video[0].play();
+            $('.centered-play-button').hide();
+		}
+		else {
+			$('.btnPlay').removeClass('paused');
+			video[0].pause();
+            $('.centered-play-button').show();
+		}
+	};
+
+	//fullscreen button clicked
+	$('.btnFS').on('click', function() {
+        if(video[0].webkitEnterFullscreen) {
+            video[0].webkitEnterFullscreen();
+        }
+        else if (video[0].mozRequestFullScreen) {
+            video[0].mozRequestFullScreen();
+        }
+        else if (video[0].msFullscreenElement) {
+            video[0].msFullscreenElement();
+        }
+        else if (video[0].fullscreenElement) {
+            video[0].fullscreenElement();
+        }
+        else {
+            alert('Your browsers doesn\'t support fullscreen');
+        }
+	});
+	
+	//sound button clicked
+	$('.sound').click(function() {
+		video[0].muted = !video[0].muted;
+		$(this).toggleClass('muted');
+		if(video[0].muted) {
+			$('.volumeBar').css('width',0);
+		}
+		else{
+			$('.volumeBar').css('width', video[0].volume*100+'%');
+		}
+	});
+	
+	//VIDEO EVENTS
+	//video canplay event
+	video.on('canplay', function() {
+		$('.loading').fadeOut(100);
+	});
+	
+	//video canplaythrough event
+	//solve Chrome cache issue
+	var completeloaded = false;
+	video.on('canplaythrough', function() {
+		completeloaded = true;
+	});
+	
+	//video ended event
+	video.on('ended', function() {
+		$('.btnPlay').removeClass('paused');
+		video[0].pause();
+	});
+
+	//video seeking event
+	video.on('seeking', function() {
+		//if video fully loaded, ignore loading screen
+		if(!completeloaded) { 
+			$('.loading').fadeIn(200);
+		}	
+	});
+	
+	//video seeked event
+	video.on('seeked', function() { });
+	
+	//video waiting for more data event
+	video.on('waiting', function() {
+		$('.loading').fadeIn(200);
+	});
+	
+	//VIDEO PROGRESS BAR
+	//when video timebar clicked
+	var timeDrag = false;	/* check for drag event */
+	$('.progress').on('mousedown', function(e) {
+		timeDrag = true;
+		updatebar(e.pageX);
+	});
+	$(document).on('mouseup', function(e) {
+		if(timeDrag) {
+			timeDrag = false;
+			updatebar(e.pageX);
+		}
+	});
+	$(document).on('mousemove', function(e) {
+		if(timeDrag) {
+			updatebar(e.pageX);
+		}
+	});
+	var updatebar = function(x) {
+		var progress = $('.progress');
+		
+		//calculate drag position
+		//and update video currenttime
+		//as well as progress bar
+		var maxduration = video[0].duration;
+		var position = x - progress.offset().left;
+		var percentage = 100 * position / progress.width();
+		if(percentage > 100) {
+			percentage = 100;
+		}
+		if(percentage < 0) {
+			percentage = 0;
+		}
+		$('.timeBar').css('width',percentage+'%');	
+		video[0].currentTime = maxduration * percentage / 100;
+	};
+
+	//VOLUME BAR
+	//volume bar event
+	var volumeDrag = false;
+	$('.volume').on('mousedown', function(e) {
+		volumeDrag = true;
+		video[0].muted = false;
+		$('.sound').removeClass('muted');
+		updateVolume(e.pageX);
+	});
+	$(document).on('mouseup', function(e) {
+		if(volumeDrag) {
+			volumeDrag = false;
+			updateVolume(e.pageX);
+		}
+	});
+	$(document).on('mousemove', function(e) {
+		if(volumeDrag) {
+			updateVolume(e.pageX);
+		}
+	});
+	var updateVolume = function(x, vol) {
+		var volume = $('.volume');
+		var percentage;
+		//if only volume have specificed
+		//then direct update volume
+		if(vol) {
+			percentage = vol * 100;
+		}
+		else {
+			var position = x - volume.offset().left;
+			percentage = 100 * position / volume.width();
+		}
+		
+		if(percentage > 100) {
+			percentage = 100;
+		}
+		if(percentage < 0) {
+			percentage = 0;
+		}
+		
+		//update volume bar and video volume
+		$('.volumeBar').css('width',percentage+'%');
+		video[0].volume = percentage / 100;
+		
+		//change sound icon based on volume
+		if(video[0].volume == 0){
+			$('.sound').removeClass('sound2').removeClass('sound3').addClass('muted');
+		}
+        else if(video[0].volume > 0 && video[0].volume < 0.6){
+            $('.sound').removeClass('muted').removeClass('sound3').addClass('sound2');
+        }
+        else if(video[0].volume > 0.6){
+			$('.sound').removeClass('sound2').removeClass('muted').addClass('sound3');
+		}
+
+	};
+
+	//Time format converter - 00:00
+	var timeFormat = function(seconds){
+		var m = Math.floor(seconds/60)<10 ? "0"+Math.floor(seconds/60) : Math.floor(seconds/60);
+		var s = Math.floor(seconds-(m*60))<10 ? "0"+Math.floor(seconds-(m*60)) : Math.floor(seconds-(m*60));
+		return m+":"+s;
+	};
+    $('.duration').text(timeFormat(video[0].duration));
+    updateVolume(0, 0.7);
+
+}
