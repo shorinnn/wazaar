d581c00ba3322789885d7558da05ff044aa91799
shorinnn <shorinnn@yahoo.com>
2015-03-12 21:36:28 +0200

    AssignedInstructor Functional tests

--- laravel/tests/functional/courses/MyCoursesCest.php
+++ laravel/tests/functional/courses/MyCoursesCest.php
@@ -34,6 +34,36 @@ class MyCourseCest{
         $I->seeCurrentUrlEquals('/courses/mycourses');
     }
     
+    public function notSeeCourseAsSecondInstructor(FunctionalTester $I){
+        $I->dontSeeAuthentication();
+        $instructor = Instructor::where('username', 'instructor')->first();
+        $second_instructor = Instructor::where('username', 'second_instructor')->first();
+        $I->amLoggedAs($second_instructor);
+        
+        $I->assertTrue( $second_instructor->hasRole('Instructor') );
+        $I->seeAuthentication();
+        $I->amOnPage('/courses/mycourses');
+        $I->seeCurrentUrlEquals('/courses/mycourses');
+        
+        $I->dontSee( $instructor->courses->first()->name );
+    }
+    
+    public function seeCourseAsSecondInstructor(FunctionalTester $I){
+        $I->dontSeeAuthentication();
+        $instructor = Instructor::where('username', 'instructor')->first();
+        $second_instructor = Instructor::where('username', 'second_instructor')->first();
+        $I->amLoggedAs($second_instructor);
+        
+        $course = $instructor->courses->first();
+        $course->assigned_instructor_id = $second_instructor->id;
+        $I->assertTrue( $course->updateUniques() );
+        $I->assertTrue( $second_instructor->hasRole('Instructor') );
+        $I->seeAuthentication();
+        $I->amOnPage('/courses/mycourses');
+        $I->seeCurrentUrlEquals('/courses/mycourses');
+        $I->see( $course->name );
+    }
+    
 //    public function seeCoursesViewEditDeletePagination(FunctionalTester $I){
 //        $instructor = Instructor::where('username', 'instructor')->first();
 //        $I->amLoggedAs($instructor);
@@ -118,12 +148,17 @@ class MyCourseCest{
      
      public function setDiscount(FunctionalTester $I){
         $course = Course::where('name','App Development')->first();
+
         $instructor = Instructor::where('username', 'instructor')->first();
         $I->amLoggedAs($instructor);
         $I->amOnPage('/courses/'.$course->slug.'/edit');
-        $I->fillField(['name' => 'sale'], '5');
-        $I->click('Update');
+        $I->seeInField(['name' => 'sale'], '0');
+//        $I->fillField(['name' => 'sale'], '5');
+        $I->submitForm('form', ['sale' => '5', 'sale_kind' => 'amount', 'name' => 'sorin']);
         $I->see('Course Updated');
+        $course = Course::find( $course->id );
+        $I->assertEquals('sorin', $course->name);
+        $I->assertEquals(5, $course->sale);
         $I->seeInField(['name' => 'sale'], '5');
     }
     
@@ -132,8 +167,9 @@ class MyCourseCest{
         $instructor = Instructor::where('username', 'instructor')->first();
         $I->amLoggedAs($instructor);
         $I->amOnPage('/courses/'.$course->slug.'/edit');
-        $I->fillField(['name' => 'sale'], '-5');
-        $I->selectOption(['name' => 'sale_kind'], 'amount');
+//        $I->fillField(['name' => 'sale'], '-5');
+        $I->submitForm('form', ['sale' => '-5', 'sale_kind' => 'amount']);
+//        $I->selectOption(['name' => 'sale_kind'], 'amount');
         $I->click('Update');
         $I->see('Could not save Course');
     }
@@ -143,8 +179,9 @@ class MyCourseCest{
         $instructor = Instructor::where('username', 'instructor')->first();
         $I->amLoggedAs($instructor);
         $I->amOnPage('/courses/'.$course->slug.'/edit');
-        $I->fillField(['name' => 'sale'], '999999999');
-        $I->selectOption(['name' => 'sale_kind'], 'amount');
+//        $I->fillField(['name' => 'sale'], '999999999');
+        $I->submitForm('form', ['sale' => '999999999', 'sale_kind' => 'amount']);
+//        $I->selectOption(['name' => 'sale_kind'], 'amount');
         $I->click('Update');
         $I->see('Could not save');
         $I->dontSeeInField(['name' => 'sale'], '999999999.00');
@@ -155,8 +192,9 @@ class MyCourseCest{
         $instructor = Instructor::where('username', 'instructor')->first();
         $I->amLoggedAs($instructor);
         $I->amOnPage('/courses/'.$course->slug.'/edit');
-        $I->fillField(['name' => 'sale'], '200');
-        $I->selectOption(['name' => 'sale_kind'], 'percentage');
+//        $I->fillField(['name' => 'sale'], '200');
+//        $I->selectOption(['name' => 'sale_kind'], 'percentage');
+        $I->submitForm('form', ['sale' => '200', 'sale_kind' => 'percentage']);
         $I->click('Update');
         $I->see('Could not save');
         $I->dontSeeInField(['name' => 'sale'], '200.00');
