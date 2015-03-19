f47bd365159385fe9bfb3ae791d8905222baa8b0
Albert Maranian Jr <albertmaranian@gmail.com>
2015-03-19 12:29:05 +0000

    Lesson video on load thumbnail

--- laravel/app/views/courses/blocks/video.blade.php
+++ laravel/app/views/courses/blocks/video.blade.php
@@ -54,7 +54,7 @@
 			$('.lesson-options-{{$lessonId}}').find('#video-thumb-container').css('display', 'block');
 			
 			$('.lesson-options-{{$lessonId}}').find('#video-thumb-container').html("<P></P><a href='#' class='fa fa-eye' data-toggle='modal' data-target='#myModal'></a> <img src='{{$video->formats[0]->thumbnail}}'/>");
-			//$('.lesson-options-{{$lessonId}}').find('#video-thumb-container p').text(timeFormat({{$video->formats[0]->duration}}));
+			$('.lesson-options-{{$lessonId}}').find('#video-thumb-container p').text(timeFormat("{{$video->formats[0]->duration}}"));
 		@endif
 		
 
