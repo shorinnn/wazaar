e949d62a2de06f6dba2b5028fe194b4f67f0ab06
Macclinson <macclinson@yahoo.com>
2015-03-19 08:29:58 +0100

    Live tweaking

--- laravel/app/views/courses/form.blade.php
+++ laravel/app/views/courses/form.blade.php
@@ -418,6 +418,14 @@
             videoLookup.initialize(function ($lessonId, $videoId){
                 $.post('/lessons/blocks/' + $lessonId + '/video/assign', {videoId : $videoId}, function (){
 
+					videoUploader.getVideo($videoId, function ($video){ 
+						$('.lesson-options-' + $lessonId).find(
+							'#video-thumb-container').html(
+							"<P></P><a href='#' class='fa fa-eye' data-toggle='modal' data-target='#myModal'></a> <img src='" + $video.formats[0].thumbnail +"'/>");
+							
+						$('.lesson-options-' + $lessonId).find('#video-thumb-container p').text(timeFormat(videoDuration));				
+					});
+
                     $('#video-link-' + $lessonId).trigger('click');
                 });
             });
