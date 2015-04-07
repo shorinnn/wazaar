0ffee9c5e26ab175db3143f63058a425c2faea8d
Albert Maranian Jr <albertmaranian@gmail.com>
2015-04-07 03:26:30 +0000

    Fix 0 division for conversions

--- laravel/app/views/analytics/partials/courseConversions.blade.php
+++ laravel/app/views/analytics/partials/courseConversions.blade.php
@@ -7,7 +7,7 @@
                 <a href="{{url('dashboard/course/'. $course['product_id'] .'/stats')}}">{{$course['name']}}</a>
 
                 <em>
-                    @if ($course['purchases'] ==0 AND $course['hits'] == 0)
+                    @if ($course['purchases'] == 0 OR $course['hits'] == 0)
                         0
                     @else
                         {{number_format($course['purchases'] / $course['hits'] * 100,2)}}
