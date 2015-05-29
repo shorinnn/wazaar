dd839b4d365b3a5c3f3c86fd301b8fbe4993f2b2
shorinnn <shorinnn@yahoo.com>
2015-05-29 20:53:29 +0300

    detect environment via .env file

--- laravel/bootstrap/start.php
+++ laravel/bootstrap/start.php
@@ -26,6 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 	'local' => array('homestead'),
+        'staging' => ['cocorium.com', 'affiliates.cocorium.com', 'orders.cocorium.com']
 ));
 
 /*
