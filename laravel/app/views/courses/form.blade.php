234d1cbdc43551b8b696ba2b33e91ef0316706f5
shorinnn <shorinnn@yahoo.com>
2015-06-01 22:04:05 +0300

    course edit debugging

--- laravel/app/views/courses/form.blade.php
+++ laravel/app/views/courses/form.blade.php
@@ -87,7 +87,7 @@
                                 </div>
                                 <div class="col-xs-12 col-sm-12 col-md-4">
                                     <div class="who-for">
-                                        {{ dd($course->requirements) }}
+                                        {{ dd( json2Array($course->requirements) ) }}
                                         <h3>{{ trans('courses/create.course-requirements') }}</h3>
                                         @foreach( json2Array($course->requirements) as $for)
                                             <ul>
