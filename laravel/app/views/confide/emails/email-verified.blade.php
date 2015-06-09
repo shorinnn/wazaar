4581d39317739b88bfc2f1b9497f3b6377947e82
Macclinson <macclinson@yahoo.com>
2015-06-09 16:33:05 +0100

    Email Verification and Registration Success pages

--- laravel/app/views/confide/emails/email-verified.blade.php
+++ laravel/app/views/confide/emails/email-verified.blade.php
@@ -1,10 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
+@extends('layouts.default')
+@section('content')
 
-<body>
-</body>
-</html>
+@if (Session::get('notice'))
+    <div class="alert">{{ Session::get('notice') }}</div>
+@endif
+<section class="container-fluid email-confirmation-pages">
+	<div class="container">
+    	<div class="row congrats-message">
+        	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            
+            </div>
+        </div>
+        <div class="row verify-email email-verified">
+        	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-md-offset-3 col-lg-offset-3">
+            	<span class="step-number">1</span>
+                <h3>Please verify your email</h3>
+                <p>We have sent verification link to your email saulius@mail.com!</p>
+            </div>
+        	<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
+            	<a href="#" class="blue-button large-blue-button">VERIFY EMAIL</a>
+            </div>
+        </div>
+        <div class="row invite-friends">
+        	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            	<span class="step-number">2</span>            
+            </div>
+        	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            	<span class="step-number">2</span>            
+            </div>
+        </div>
+    </div>
+</section>
+
+@stop
