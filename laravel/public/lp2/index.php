e9d6cec9d432e9150e5e6de1ed46d777c29578b8
shorinnn <shorinnn@yahoo.com>
2015-06-15 13:04:01 +0300

    temporary aweber form for lp links

--- laravel/public/lp2/index.php
+++ laravel/public/lp2/index.php
@@ -424,7 +424,8 @@ Wazaarのプロジェクトリーダーをやるということもありませ�
 <!-- form -->
 <div class="form_bg">
 	<div class="container">
-        <form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/lp';?>"  >
+        <!--<form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/lp';?>"  >-->
+            <form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl"  >
 <div style="display: none;">
     <input type='hidden' name='stpi' value='<?php echo @$_GET['stpi'];?>' />
 <input type="hidden" name="meta_web_form_id" value="2051478497" />
@@ -524,7 +525,8 @@ Wazaarのミッションになります。
 <!-- form -->
 <div class="form_bg">
 	<div class="container">
-        <form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/lp';?>"  >
+        <!--<form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="<?php echo 'http://'.$_SERVER['HTTP_HOST'].'/lp';?>"  >-->
+            <form method="post" class="af-form-wrapper" accept-charset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl"  >
 <div style="display: none;">
 <input type='hidden' name='stpi' value='<?php echo @$_GET['stpi'];?>' />
 <input type="hidden" name="meta_web_form_id" value="2051478497" />
