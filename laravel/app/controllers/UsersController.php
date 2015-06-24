de0f1ae1aacf782195d6e96b62edf5f14b282442
shorinnn <shorinnn@yahoo.com>
2015-06-24 23:49:42 +0300

    debugging

--- laravel/app/controllers/UsersController.php
+++ laravel/app/controllers/UsersController.php
@@ -292,6 +292,7 @@ class UsersController extends Controller
                         Cookie::queue('stpi', null, -1);
                         $this->users->saveSocialPicture($user, "FB$result[id]", "https://graph.facebook.com/$result[id]/picture?type=large");
                         //user created
+                        dd($user);
                         Auth::login($user);
                         if($user->is_second_tier_instructor=='yes') return Redirect::action('UsersController@links');
                         else return Redirect::intended('/');
