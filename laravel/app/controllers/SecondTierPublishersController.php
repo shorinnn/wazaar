23c3696ebf4989493d26967b644613d5bebe9ec3
shorinnn <shorinnn@yahoo.com>
2015-06-29 13:41:32 +0300

    STP approval email https://trello.com/c/3lXuZ9xB/98-2-tier-publisher-approval-email

--- laravel/app/controllers/SecondTierPublishersController.php
+++ laravel/app/controllers/SecondTierPublishersController.php
@@ -27,7 +27,24 @@ class SecondTierPublishersController extends \BaseController {
 	{
             $user = User::find($id);
             $user->sti_approved = trim( strtolower( Input::get('value') ) );
-           if( $user->updateUniques() ){
+            if( $user->updateUniques() ){
+                if( Input::get('value')=='Yes' ){
+                    // send STP approval email
+                    $name = '';
+                    $instructor = Instructor::find($user->id);
+                    if($instructor->profile != null) $name = $instructor->profile->last_name.' '.$instructor->profile->first_name;
+                    else $name = "$user->last_name $user->first_name";
+                    
+                    Mail::send(
+                        'emails.second_tier_approved',
+                        compact('name'),
+                        function ($message) use ($user, $name) {
+                            $message
+                                ->to($user->email, $name)
+                                ->subject( '販売者２ティアのリンク発行承認のご連絡' );
+                        }
+                    );
+                }
                 if(Request::ajax()) return json_encode( ['status'=>'success','eh' => Input::get('value') ] );
                 return Redirect::back()->withSuccess( trans('crud/errors.object_updated', ['object'=>'User'] ));
             }
