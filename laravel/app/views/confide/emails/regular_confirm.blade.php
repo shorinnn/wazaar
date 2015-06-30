74603925999c531b4669d6d39ee868c2bb698ac3
shorinnn <shorinnn@yahoo.com>
2015-06-30 18:59:44 +0300

    email verification text https://trello.com/c/Ao4bhFY3/112-verification-email-instructors

--- laravel/app/views/confide/emails/regular_confirm.blade.php
+++ laravel/app/views/confide/emails/regular_confirm.blade.php
@@ -1,17 +1,17 @@
-<p> {{ $lastName }} 様 </p>
+<p>こんにちは</p>
 
 <p>Wazaarへようこそ！</p>
 
-<p>メールアドレスの確認のために、下記のリンクをクリックしてください。</p>
+<p> メールアドレスの確認のために、<br />
+    下記のリンクをクリックしてください。
+</p>
 
 <a href='{{{ action("UsersController@confirm", $user['confirmation_code']) }}}'>
     {{{ action("UsersController@confirm", $user['confirmation_code']) }}}
 </a>
 
 <p>
-ワザールではまだまだ動画教材が足りませんので、{{ $lastName }} 様の動画教材を是非、<br />
-ワザールにご投稿ください。<br />
-今後とも何卒よろしくお願い致します。<br />
+今後ともよろしくお願い致します。<br />
 ワザール　日本法人代表<br />
-峯山<br />
+峯山
 </p>
