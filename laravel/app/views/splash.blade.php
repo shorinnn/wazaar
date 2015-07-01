b33a37b10c638fba5406e30f61b2923d20523df2
shorinnn <shorinnn@yahoo.com>
2015-07-01 14:40:29 +0300

    aws identifier

--- laravel/app/views/splash.blade.php
+++ laravel/app/views/splash.blade.php
@@ -6,6 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>
+            {{getenv('AWS_MACHINE_IDENFITIER')}}
             @if(isset($_ENV['AWS_MACHINE_IDENFITIER']))
                 {{ $_ENV['AWS_MACHINE_IDENFITIER'] }} 
             @else
