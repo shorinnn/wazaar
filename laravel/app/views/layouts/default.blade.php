52b2c1a559b0e14b40ca1612525e980707c4b5d1
shorinnn <shorinnn@yahoo.com>
2015-07-01 16:52:18 +0300

    cleaned code

--- laravel/app/views/layouts/default.blade.php
+++ laravel/app/views/layouts/default.blade.php
@@ -4,13 +4,7 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@yield('page_title')
-            @if( getenv('AWS_MACHINE_IDENTIFIER')=='' )
-                 - Wazaar
-            @else
-                {{ getenv('AWS_MACHINE_IDENTIFIER') }} 
-            @endif
-        </title>    
+	<title>@yield('page_title') Wazaar</title>    
 	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">-->
     <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
     <!--<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'>-->
