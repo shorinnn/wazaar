894d90e2206c8dafb2e1af37122586b347684b6a
Macclinson <macclinson@yahoo.com>
2015-07-10 16:11:44 +0100

    New editor UI

--- laravel/app/views/courses/editor/step2.blade.php
+++ laravel/app/views/courses/editor/step2.blade.php
@@ -34,18 +34,43 @@
                         <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
                             <input type="text" name="module-name" placeholder="Enter module title">
                             <!--<h2>Introduction to Digital Marketing</h2>-->
-                            <p class="regular-paragraph">In this module, you will become familiar with the course, your 
+                            <!--<p class="regular-paragraph">In this module, you will become familiar with the course, your 
                             instructor, your classmates, and our learning environment. Then, you will learn how new digital 
                             tools are enabling customers to take a more active role in developing and branding the products 
                             they consume.
-                            </p>
+                            </p>-->
+                            <textarea placeholder="Enter description..."></textarea>
                         </div>
                     </div>
-                    <div class="row module-buttons">
+                    <div class="row footer-buttons">
                     	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         	<a href="#" class="green-button large-button">Save changes</a>
                         	<a href="#" class="default-button large-button">Cancel</a>
                             <a href="#" class="delete-lesson right"><i class="fa fa-trash-o"></i> Delete this lesson</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="lesson-container">
+                	<div class="row">
+                    	<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                            <span class="lesson-name">Lesson 1</span>
+                            <div class="preview-thumb">
+                            
+                                <label class="default-button large-button">
+                                    <span>Upload new</span>
+                                    <input type="file" hidden=""/>
+                                </label>
+	                            <a href="#" class="remove-video">Remove video</a>
+                            </div>
+                        </div>
+                    	<div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+                        	<input type="text" class="lesson-title" placeholder="Enter lesson title">
+                            <div>
+                            	<h6>DESCRIPTION
+                                	<span class="lead">360 Characters left</span>
+                                </h6>
+                                <textarea placeholder="Enter short lesson description..."></textarea>
+                            </div>
                         </div>
                     </div>
                 </div>
