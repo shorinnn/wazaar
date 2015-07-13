ad880907ec78a1314e6f09e136c039009077639b
Albert Maranian <albertmaranian@gmail.com>
2015-07-13 13:01:00 +0000

    Preset Update

--- laravel/app/config/wazaar.php
+++ laravel/app/config/wazaar.php
@@ -3,13 +3,10 @@
 return [
     'AWS_VIDEO_PRESETS' =>
         [
-            /*'1351620000001-100020' => 'iPhone 4s and above, iPad 3G and above, iPad mini, Samsung Galaxy S2/S3/Tab 2', //mobile
-            '1351620000001-000040' => 'Generic 360p 16:9 Resolution' , //tablet
-            '1351620000001-100070' => 'Facebook, SmugMug, Vimeo, YouTube' //desktop*/
-            //'1421660966371-o9l23s' => 'Custom Preset for Mobile Devices',
+
             //'1436779339512-wtkz9d' => 'Custom Preset for Mobile Devices',
-            //'1421661161826-cx6nmz' => 'Custom Preset for Desktop Devices'
-            '1436779339512-wtkz9d' => 'Custom Preset for Mobile Devices',
+
+            '1436790010046-k9rw9e' => 'Custom Preset for Mobile Devices',
             '1436790010046-k9rw9e' => 'Custom Preset for Desktop Devices'
         ],
     'AWS_WEB_DOMAIN' => 'd2t29bdxhtuuvy.cloudfront.net',
