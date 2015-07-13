b3900326dc31be1354efad37530dd2fe58353671
Albert Maranian <albertmaranian@gmail.com>
2015-07-13 13:27:46 +0000

    Added low reso preset

--- laravel/app/config/wazaar.php
+++ laravel/app/config/wazaar.php
@@ -4,7 +4,8 @@ return [
     'AWS_VIDEO_PRESETS' =>
         [
             '1436790010046-k9rw9e' => 'Custom Preset for Desktop Devices',
-            '1436779339512-wtkz9d' => 'Custom Preset for Mobile Devices'
+            '1436779339512-wtkz9d' => 'Custom Preset for Mobile Devices',
+            '1436790034239-byka2c' => 'Low Resolution'
         ],
     'AWS_WEB_DOMAIN' => 'd2t29bdxhtuuvy.cloudfront.net',
     'S3_PROFILES_BUCKET' => 'profile_pictures',
