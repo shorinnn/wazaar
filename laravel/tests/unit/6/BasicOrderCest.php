5813e07f407f9d85b9f8e17ad59c6ad68fef45a5
shorinnn <shorinnn@yahoo.com>
2015-07-16 18:52:05 +0300

    ltc affiliate tests

--- laravel/tests/unit/6/BasicOrderCest.php
+++ laravel/tests/unit/6/BasicOrderCest.php
@@ -570,4 +570,57 @@ class BasicOrderCest{
         $I->assertEquals( $purchase->second_tier_instructor_earnings, 20 * 0.02 );
         $I->assertEquals( $purchase->site_earnings, 20 - 20 * 0.03 - 20 * 0.02 );
     }
+    
+    public function LTCStaysTheSameEvenIfMoreThan30Days(UnitTester $I){
+        $student = Student::where('username','student')->first();
+        $student->ltc_affiliate_id = 2;
+        $student->created_at = '2010-01-01 10:10:10';
+        $I->assertTrue( $student->updateUniques() );
+        DB::table('purchases')->delete();
+        
+        $course = Course::first();
+        $course->price = 1050;
+        $course->affiliate_percentage = 0;
+        $course->updateUniques();
+        DB::table('courses')->where('id', $course->id)->update(['price' => '105']);
+        $course = Course::first();
+        
+        $I->assertEquals(105, $course->price);
+        $data = [];
+        $data['successData']['REF'] = '123';
+        $data['successData']['processor_fee'] = '5';
+        $data['successData']['tax'] = '10';
+        $data['successData']['giftID'] = null;
+        $data['successData']['ORDERID'] = 1;
+        $data['successData']['balance_used'] = '0';
+        $data['successData']['balance_transaction_id'] = '0';
+        
+        $I->assertEquals( 2, $student->ltc_affiliate_id );
+        $I->assertLessThan( 1, $course->instructor->second_tier_instructor_id );
+        $I->assertNotEquals( false, $student->purchase($course, null, $data) );
+        
+        $purchase = Purchase::orderBy('id','desc')->first();
+        
+        $I->assertLessThan( 1, $purchase->product_affiliate_id);
+        $I->assertEquals( 2, $purchase->ltc_affiliate_id);
+        $I->assertLessThan( 1, $purchase->second_tier_affiliate_id);
+        $I->assertLessThan( 1, $purchase->second_tier_instructor_id);
+                
+        $I->assertEquals( $purchase->purchase_price, 105 );
+        $I->assertEquals( $purchase->original_price, 105 );
+        $I->assertEquals( $purchase->discount_value, 0 );
+        $I->assertEquals( $purchase->discount, null );
+        $I->assertEquals( $purchase->processor_fee, 5 );
+        $I->assertEquals( $purchase->tax, 10 );
+        $I->assertEquals( $purchase->instructor_earnings, 68 );     
+        $I->assertEquals( $purchase->affiliate_earnings, 0 );
+        $I->assertEquals( $purchase->ltc_affiliate_earnings, 32 * 0.03 );
+        $I->assertEquals( $purchase->second_tier_affiliate_earnings, 0 );
+        $I->assertEquals( $purchase->second_tier_instructor_earnings, 0 );
+        // no second tier aff, wazaar cut goes up to 32%
+        $I->assertEquals( $purchase->site_earnings, 32 - 32 * 0.03 );
+        
+        $student = Student::where('username','student')->first();
+        $I->assertEquals( 2, $student->ltc_affiliate_id);
+    }
 }
