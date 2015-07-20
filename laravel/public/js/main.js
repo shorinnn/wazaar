f0a22ee52a8a67e5b55fe2a07ce540687f6d11ce
shorinnn <shorinnn@yahoo.com>
2015-07-20 22:33:12 +0300

    desc page video changes https://trello.com/c/wGMCuHOR/195-course-description-video

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -1099,6 +1099,7 @@ function skinVideoControls(){
 		return m+":"+s;
 	};
         console.log('DURATION IS  '+video[0].duration);
+        if( video[0].duration == 'NaN') console.log('IS NAN MAYNE');
     $('.duration').text(timeFormat(video[0].duration));
     updateVolume(0, 0.7);
     return true;
