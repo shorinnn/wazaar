90f196f1bff110d19f74aa505db15650ba42cdfa
Albert Maranian <albertmaranian@gmail.com>
2015-07-29 13:46:33 +0000

    Progress bar for course video upload

--- laravel/app/views/courses/editor/step1.blade.php
+++ laravel/app/views/courses/editor/step1.blade.php
@@ -377,6 +377,7 @@
                 alert(uploadErrors.join("\n"));
                 return false;
             } else {
+                $('.course-video-upload-button-progress').find('.progress').show();
                 data.submit();
             }
             window.reloadConfirm = true;
@@ -395,7 +396,7 @@
 
             setInterval(function(){
                 count++;
-                document.getElementById('video-transcoding-indicator-course-description').innerHTML = _("Video Currently Processing") + new Array(count % 4).join('.');
+                //document.getElementById('video-transcoding-indicator-course-description').innerHTML = _("Video Currently Processing") + new Array(count % 4).join('.');
             }, 500);
 
             if ($data.jqXHR.status == 201){
