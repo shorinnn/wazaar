d533c74942e3ddaf4d167eadf297aaf9d09411a6
shorinnn <shorinnn@yahoo.com>
2015-08-05 17:46:30 +0300

    https://trello.com/c/SvpHEyJo/257-editor-sidebar-links

--- laravel/app/views/courses/editor/v2/module_li.blade.php
+++ laravel/app/views/courses/editor/v2/module_li.blade.php
@@ -1,5 +1,6 @@
 <li class="shr-editor-module-{{$module->id}}">
-    <a class='shr-editor-module-{{$module->id}}-copy-name' href="#">{{ $module->name }}</a>
+    <a class='shr-editor-module-{{$module->id}}-copy-name scroll-to-element' href="#" data-target='div.shr-editor-module-{{$module->id}}'>
+        {{ $module->name }}</a>
     <ol class='drag-lesson'>
         @foreach($module->lessons()->orderBy('order','asc')->get() as $lesson)
             {{ View::make('courses.editor.v2.lesson_li')->with( compact('lesson') ) }}
