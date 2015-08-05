38b783e30a857614261f67a229eb192e759b0d14
shorinnn <shorinnn@yahoo.com>
2015-08-05 11:00:56 +0300

    classroom free preview check

--- laravel/app/controllers/ClassroomController.php
+++ laravel/app/controllers/ClassroomController.php
@@ -84,7 +84,7 @@ class ClassroomController extends \BaseController {
                 return Redirect::to('/');
             }
             if($student->id != $course->instructor_id && $student->id != $course->assigned_instructor_id ){
-                if( (!$student->purchased($course) && $purchase==null && $lesson->price==0) || ( !$student->purchased($course) && $purchase->free_product=='yes') ){
+                if( (!$student->purchased($course) && $purchase==null && $lesson->free_preview=='yes') || ( !$student->purchased($course) && $purchase->free_product=='yes') ){
                     return View::make('courses.classroom.crash_lesson')->with( compact('course') )->with( compact('lesson') )->with( compact('video') );
                 }
             }
