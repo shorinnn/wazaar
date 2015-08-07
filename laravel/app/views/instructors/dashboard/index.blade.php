092156df89503accfe1340204a271f0576877244
Albert Maranian <albertmaranian@gmail.com>
2015-08-07 13:52:18 +0000

    Instructor analytics 3 boxes can now be filtered

--- laravel/app/views/instructors/dashboard/index.blade.php
+++ laravel/app/views/instructors/dashboard/index.blade.php
@@ -23,23 +23,23 @@
                     <div>
                         <div class="dropdown-wrapper">
                             <button class="btn btn-default" type="button">
-                                {{trans('analytics.sales')}} <span id="header-top-courses-frequency">{{trans('analytics.today')}}</span></button>
-                            <ul id="activities-dropdown" aria-labelledby="btnGroupDrop2" role="menu" class="dropdown-menu top-courses-dropdown">
+                                {{trans('analytics.sales')}} <span id="header-sales-frequency">{{trans('analytics.today')}}</span></button>
+                            <ul id="activities-dropdown" aria-labelledby="btnGroupDrop2" role="menu" class="dropdown-menu sales-dropdown">
                                 <li>
-                                    <a class="active with-today" href="#" onclick="Analytics.topCourses('daily','', this); return false;">{{trans('analytics.today')}}</a>
+                                    <a class="with-today" href="#" onclick="Analytics.Sales('daily','','',this); return false;">{{trans('analytics.today')}}</a>
                                 </li>
                                 <li>
-                                    <a class="with-weekly" href="#" onclick="Analytics.topCourses('week','', this); return false;">{{trans('analytics.thisWeek')}}</a>
+                                    <a class="with-weekly" href="#" onclick="Analytics.Sales('week','','',this); return false;">{{trans('analytics.thisWeek')}}</a>
                                 </li>
                                 <li>
-                                    <a class="with-monthly" href="#" onclick="Analytics.topCourses('month','', this); return false;">{{trans('analytics.thisMonth')}}</a>
+                                    <a class="with-monthly" href="#" onclick="Analytics.Sales('month','','',this); return false;">{{trans('analytics.thisMonth')}}</a>
                                 </li>
                                 <li>
-                                    <a class="with-alltime" href="#" onclick="Analytics.topCourses('alltime','', this); return false;">{{trans('analytics.allTime')}}</a>
+                                    <a class="with-alltime" href="#" onclick="Analytics.Sales('alltime','','',this); return false;">{{trans('analytics.allTime')}}</a>
                                 </li>
                             </ul>
                         </div>
-                        <ul id="wrapper-top-courses">
+                        <ul id="wrapper-sales">
                             {{$salesView}}
                         </ul>
                     </div>
@@ -52,24 +52,24 @@
                     <div id="sales-today">
                         <div class="dropdown-wrapper">
                             <button aria-expanded="false" data-toggle="dropdown" class="btn btn-default dropdown-toggle" id="btnGroupDrop3" type="button">
-                                {{trans('analytics.salesCount')}} <span id="header-sales-frequency">{{trans('analytics.today')}}</span></button>
-                            <ul id="activities-dropdown" aria-labelledby="btnGroupDrop3" role="menu" class="dropdown-menu sales-dropdown">
+                                {{trans('analytics.salesCount')}} <span id="header-sales-count-frequency">{{trans('analytics.today')}}</span></button>
+                            <ul id="activities-dropdown" aria-labelledby="btnGroupDrop3" role="menu" class="dropdown-menu sales-count-dropdown">
                                 <li>
-                                    <a class="active" href="#" onclick="Analytics.sales('daily','','', this); return false;">{{trans('analytics.today')}}</a>
+                                    <a class="active" href="#" onclick="Analytics.SalesCount('daily', this); return false;">{{trans('analytics.today')}}</a>
                                 </li>
                                 <li>
-                                    <a class="" href="#" onclick="Analytics.sales('week','','', this); return false;">{{trans('analytics.thisWeek')}}</a>
+                                    <a class="" href="#" onclick="Analytics.SalesCount('week', this); return false;">{{trans('analytics.thisWeek')}}</a>
                                 </li>
                                 <li>
-                                    <a class="" href="#" onclick="Analytics.sales('month','','', this); return false;">{{trans('analytics.thisMonth')}}</a>
+                                    <a class="" href="#" onclick="Analytics.SalesCount('month', this); return false;">{{trans('analytics.thisMonth')}}</a>
                                 </li>
                                 <li>
-                                    <a class="" href="#" onclick="Analytics.sales('alltime','','', this); return false;">{{trans('analytics.alltime')}}</a>
+                                    <a class="" href="#" onclick="Analytics.SalesCount('alltime', this); return false;">{{trans('analytics.alltime')}}</a>
                                 </li>
                             </ul>
                         </div>
 
-                        <div id="wrapper-sales">
+                        <div id="wrapper-sales-count">
                             {{$salesCountView}}
                         </div>
 
@@ -122,84 +122,8 @@
                             </div>
                         </form>
                     </div>
-
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th>Rank</th>
-                                <th>Username</th>
-                                <th>Full name</th>
-                                <th>Sales (#)</th>
-                                <th class="text-right last-column sorting-active relative"> Sales (¥)<i class="fa fa-arrow-down"></i></th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td class="link">WazaarAffiliate</td>
-                                <td>Affialiate Wazaar	</td>
-                                <td>57</td>
-                                <td class="text-right last-column">  ¥ 23,624</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="first-column">2</th>
-                                <td class="link">vstreich</td>
-                                <td>Feeney Aiden</td>
-                                <td>2</td>
-                                <td class="text-right last-column">¥ 64</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="first-column">3</th>
-                                <td class="link">affiliate</td>
-                                <td>Instructor Wazaar</td>
-                                <td>3</td>
-                                <td class="text-right last-column">  ¥ 50</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="first-column">4</th>
-                                <td class="link">eloise88</td>
-                                <td>Corkery Milford</td>
-                                <td>4</td>
-                                <td class="text-right last-column">¥ 43</td>
-                            </tr>
-                            <tr>
-                                <th scope="row" class="first-column">5</th>
-                                <td class="link">WazaarAffiliate	</td>
-                                <td>Affialiate Wazaar	</td>
-                                <td>4</td>
-                                <td class="text-right last-column">¥ 41</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="table-pagination clearfix">
-                        <ul>
-                            <li class="prev">
-                                <a href="#"><i class="wa-chevron-left"></i></a>
-                            </li>
-                            <li class="active">
-                                <a href="#">1</a>
-                            </li>
-                            <li>
-                                <a href="#">2</a>
-                            </li>
-                            <li>
-                                <a href="#">3</a>
-                            </li>
-                            <li>
-                                <a href="#">4</a>
-                            </li>
-                            <li>
-                                <a href="#">5</a>
-                            </li>
-                            <li>
-                                <a href="#">6</a>
-                            </li>
-                            <li class="next">
-                                <a href="#"><i class="wa-chevron-right"></i></a>
-                            </li>
-                        </ul>
+                    <div>
+                        {{$topAffiliates}}
                     </div>
                 </div>
             </div>
@@ -217,5 +141,5 @@
 @stop
 
 @section('extra_js')
-    <script type="text/javascript" src="{{url('js/analytics.js')}}"></script>
+    <script type="text/javascript" src="{{url('js/instructor-analytics.js')}}"></script>
 @stop
---
+++ laravel/public/js/instructor-analytics.js
@@ -0,0 +1,142 @@
+var $ajaxLoader = $('#ajax-loader-wrapper').html();
+
+var Analytics = {
+
+    'activateLoader' : function (){
+        $('#wrapper-sales').html($ajaxLoader);
+        $('#wrapper-top-courses').html($ajaxLoader);
+        $('#wrapper-tracking-codes-table').html($ajaxLoader);
+        $('#wrapper-tracking-codes').html($ajaxLoader);
+        $('#wrapper-course-conversions').html($ajaxLoader);
+        $('#wrapper-tracking-code-conversions').html($ajaxLoader);
+        $('#wrapper-ltc-registrations').html($ajaxLoader);
+        $('#wrapper-tracking-code-stats').html($ajaxLoader);
+        $('#wrapper-user-stats').html($ajaxLoader);
+        $('#wrapper-sales-count').html($ajaxLoader);
+    },
+    'topCourses' :  function ($frequency, $courseId, $elem){
+
+
+        $.get('/dashboard/topcourses/' + $frequency + '/' + $courseId, function ($html){
+            $('#wrapper-top-courses').html($html);
+
+            $('.top-courses-dropdown a').removeClass('active');
+            $($elem).addClass('active');
+            if ($frequency == 'daily'){
+                $('#header-top-courses-frequency').html(_("Today"));
+            }
+            else if($frequency == 'week'){
+                $('#header-top-courses-frequency').html(_("This Week"));
+            }
+            else if($frequency == 'month'){
+                $('#header-top-courses-frequency').html(_("This Month"));
+            }
+            else if($frequency == 'alltime'){
+                $('#header-top-courses-frequency').html(_("All Time"));
+            }
+        });
+    },
+    'Sales' :  function ($frequency, $courseId, $trackingCode, $elem){
+
+
+        $.get('/analytics/sales/' + $frequency + '/' + $courseId + '/' + $trackingCode, function ($html){
+            $('#wrapper-sales').html($html);
+
+            $('.sales-dropdown a').removeClass('active');
+            $($elem).addClass('active');
+            if ($frequency == 'daily'){
+                $('#header-sales-frequency').html(_("Today"));
+            }
+            else if($frequency == 'week'){
+                $('#header-sales-frequency').html(_("This Week"));
+            }
+            else if($frequency == 'month'){
+                $('#header-sales-frequency').html(_("This Month"));
+            }
+            else if($frequency == 'alltime'){
+                $('#header-sales-frequency').html(_("All Time"));
+            }
+            if ($('#wrapper-tracking-codes-table').length == 1) {
+                Analytics.trackingCodeTable($frequency, $courseId);
+            }
+        });
+    },
+
+    'SalesCount' : function ($frequency, $elem){
+        Analytics.activateLoader();
+        $.get('/analytics/sales/count/' + $frequency , function ($html){
+            $('.sales-count-dropdown a').removeClass('active');
+            $($elem).addClass('active');
+            if ($frequency == 'today'){
+                $('#header-sales-count-frequency').html(_("Today"));
+                $('.with-today').trigger('click');
+            }
+            else if($frequency == 'week'){
+                $('#header-sales-count-frequency').html(_("This Week"));
+                $('.with-weekly').trigger('click');
+            }
+            else if($frequency == 'month'){
+                $('#header-sales-count-frequency').html(_("This Month"));
+                $('.with-monthly').trigger('click');
+            }
+            else if($frequency == 'alltime'){
+                $('#header-sales-count-frequency').html(_("All Time"));
+                $('.with-alltime').trigger('click');
+            }
+
+            $('#wrapper-sales-count').html($html);
+        });
+    },
+
+    'trackingCodeTable' : function ($frequency, $courseId){
+
+        $.get('/analytics/trackingcodetable/' + $frequency + '/' + $courseId, function ($html){
+            $('#wrapper-tracking-codes-table').html($html);
+        });
+    },
+
+    'trackingSalesCodes' :  function ($frequency, $courseId, $elem){
+
+        $.get('/analytics/trackingcodessales/' + $frequency + '/' + $courseId, function ($html){
+            $('#wrapper-tracking-codes').html($html);
+
+            $('.tracking-codes-dropdown a').removeClass('active');
+            $($elem).addClass('active');
+            if ($frequency == 'daily'){
+                $('#header-tracking-codes-frequency').html(_("Today"));
+            }
+            else if($frequency == 'week'){
+                $('#header-tracking-codes-frequency').html(_("This Week"));
+            }
+            else if($frequency == 'month'){
+                $('#header-tracking-codes-frequency').html(_("This Month"));
+            }
+            else if($frequency == 'alltime'){
+                $('#header-tracking-codes-frequency').html(_("All Time"));
+            }
+        });
+    },
+
+    'trackingCodeStats' :  function ($frequency, $courseId, $code, $elem){
+
+        $.get('/dashboard/course/' + $courseId + '/trackingcode/' + $code + '/stats/' + $frequency, function ($html){
+            $('#wrapper-tracking-code-stats').html($html);
+
+            $('.tracking-code-hits-dropdown a').removeClass('active');
+            $($elem).addClass('active');
+            if ($frequency == 'daily'){
+                $('#header-tracking-codes-frequency').html(_("Today"));
+
+            }
+            else if($frequency == 'week'){
+                $('#header-tracking-codes-frequency').html(_("This Week"));
+            }
+            else if($frequency == 'month'){
+                $('#header-tracking-codes-frequency').html(_("This Month"));
+            }
+            else if($frequency == 'alltime'){
+                $('#header-tracking-codes-frequency').html(_("All Time"));
+            }
+        });
+    }
+};
