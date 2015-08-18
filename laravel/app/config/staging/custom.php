097d707450dcd20612a13935c19bb2d6f349178e
shorinnn <shorinnn@yahoo.com>
2015-08-18 23:09:33 +0300

    composer.json update

--- laravel/app/config/staging/custom.php
+++ laravel/app/config/staging/custom.php
@@ -16,4 +16,15 @@ return [
                    'second_tier_instructor_percentage' => 2, 'self_promotion_instructor_percentage' => 80, 'self_promotion_site_percentage' => 20 ],
     'cashout' => [ 'fee' => 15, 'threshold' => 50, 'start_date' => '01-01-2015' ],
     'first_name_first' => false,
+    'vimeo' => ['client_id' => '9b523659aa38c4861c1d6675e431da7905941c8a', 'access_token' => '8116968eb0069207a0cda07b52477840',
+            'client_secret' => 'B+IJ8cvhOyZgq0XzR+KZS0M+uNgcNm41oje+KcBThjhmcpBUQeYQv8s4z6xiMhDEB1a8yXohuoc79lBPVDLaagwyi5oer6R5h5WTVCUFHuMSVFFihUUqkMLJmpc8CgE1'],
+    'cache-expiry' => [
+        'course-desc-top-details' => 20,
+        'course-desc-bottom-details' => 60,
+        'student-dash-enrolled-image' => 60,
+        'student-dash-lesson-count' => 60,
+        'student-dash-first-lesson' => 60,
+        'student-dash-completed-course' => 60,
+        'student-dash-wishlist-course' => 60,
+    ]
 ];
