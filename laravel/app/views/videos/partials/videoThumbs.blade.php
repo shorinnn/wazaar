0a03f7edc55ea01392bf7d57995ecdbfbb3fb3e7
Albert Maranian <albertmaranian@gmail.com>
2015-08-18 14:44:16 +0000

    Boxed the video thumbs on lookup

--- laravel/app/views/videos/partials/videoThumbs.blade.php
+++ laravel/app/views/videos/partials/videoThumbs.blade.php
@@ -2,14 +2,14 @@
     <ul class="list-unstyled video-list-thumbs row radio-buttons clearfix">
         @if ($videos->count() > 0)
             @foreach($videos as $video)
-                <li class="col-lg-4 col-sm-4 col-xs-6 radio-checkbox radio-checked" id="li-video-{{$video->id}}">
+                <li class="col-lg-4 col-sm-4 col-xs-6 radio-checkbox radio-checked video-thumb-box" id="li-video-{{$video->id}}">
                     <a href="#" title="{{$video->original_filename}}">
-                        <img src="{{$video->formats[0]->thumbnail}}" alt="{{$video->original_filename}}" class="img-responsive" height="130px" />
-                        <h2>
+                        <img src="{{$video->formats[0]->thumbnail}}" alt="{{$video->original_filename}}" class="img-responsive" />
+                        <div>
                             <input type="radio" class="radio-video-id" name="radioVideoId" id="radio-video-{{$video->id}}" value="{{$video->id}}">
                             <label for="radio-video-{{$video->id}}" class="small-radio"></label>
-                            <span class="file-name">{{$video->original_filename}}</span>
-                        </h2>
+                            <span class="file-name">{{$video->trimmed_original_filename}}</span>
+                        </div>
 
                         <span class="duration">{{$video->formats[0]->duration}}</span>
                     </a>
