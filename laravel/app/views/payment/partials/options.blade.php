be27a67f46aeb9dba5267cd4e890e653d2837a0e
Albert Maranian <albertmaranian@gmail.com>
2015-08-30 02:32:07 +0000

    Completed New Payment method

--- laravel/app/views/payment/partials/options.blade.php
+++ laravel/app/views/payment/partials/options.blade.php
@@ -1,7 +1,11 @@
 <ul style="padding-left: 20px">
     @foreach(PaymentHelper::getPaymentMethods() as $payment)
         <li>
-                <div><input type="radio" name="radioPaymentOption" value="{{$payment->reference}}" /> <i class="fa fa-{{PaymentHelper::getCCIcon($payment->response['card'])}}"></i> {{$payment->response['card']}}</div>
+                <div>
+                    <input type="radio" name="radioPaymentOption" value="{{$payment->response['card_id']}}" />
+                    <i class="fa fa-{{PaymentHelper::getCCIcon($payment->response['card'])}}"></i> {{$payment->response['card']}}
+                    <a href="#" class="remove-card" data-payment-log-id="{{$payment->id}}"><i class="fa fa-times"></i></a>
+                </div>
                 <div style="margin-left: 20px">Card Number: ***********{{$payment->response['last4']}}</div>
                 <div style="margin-left: 20px">Expiry: {{$payment->response['exp_month']}}/{{$payment->response['exp_year']}}</div>
         </li>
