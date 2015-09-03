28b8339ea9b43b018e979de64faeb0537046b2b1
Macclinson <macclinson@yahoo.com>
2015-09-03 14:38:08 +0100

    New Modal box UX

--- laravel/app/views/videos/partials/videoThumbs.blade.php
+++ laravel/app/views/videos/partials/videoThumbs.blade.php
@@ -1,17 +1,20 @@
-<div class="video-thumbs">
-    <ul class="list-unstyled video-list-thumbs row radio-buttons clearfix">
+<div class="video-thumbs clearfix">
+    <ul class="list-unstyled video-list-thumbs row no-margin clearfix">
         @if ($videos->count() > 0)
             @foreach($videos as $video)
-                <li class="col-lg-4 col-sm-4 col-xs-6 radio-checkbox radio-checked video-thumb-box" id="li-video-{{$video->id}}">
+                <li class="col-xs-6 col-sm-4 col-md-3 col-lg-3 radio-checkbox radio-checked video-thumb-box" id="li-video-{{$video->id}}">
                     <a href="#" title="{{$video->original_filename}}">
-                        <img src="{{$video->formats[0]->thumbnail}}" alt="{{$video->original_filename}}" class="img-responsive" />
+                    	<div class="thumb-container relative">
+                        	<img src="{{$video->formats[0]->thumbnail}}" alt="{{$video->original_filename}}" class="img-responsive" />
+                            <span class="duration">{{$video->formats[0]->duration}}</span>
+                        </div>
                         <div>
                             <input type="radio" class="radio-video-id" name="radioVideoId" id="radio-video-{{$video->id}}" value="{{$video->id}}">
                             <label for="radio-video-{{$video->id}}" class="small-radio"></label>
                             <span class="file-name">{{$video->trimmed_original_filename}}</span>
                         </div>
 
-                        <span class="duration">{{$video->formats[0]->duration}}</span>
+                        
                     </a>
                 </li>
             @endforeach
