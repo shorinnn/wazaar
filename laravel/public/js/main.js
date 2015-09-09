88c224e5dd51f711d8fed8236356ce0136ef175b
Arthur <arthur_yap_18@yahoo.com>
2015-09-09 16:49:02 +0800

    working on discount coutdown timer 8

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -99,10 +99,13 @@ function discountCountdown(target, time_end){
 
 
     setInterval(function () {
-        console.log(time_end);
         var now = moment().format('X');
         var diffTime = time_end - now; //better to handle this in Controller to avoid timezone problem
         var duration = moment.duration(diffTime, 'seconds');
+
+        console.log(time_end+' time ends');
+        console.log(now+' now time');
+        console.log(duration+'time diff in secs');
         if(duration.days() >= 1){
             $(target).text(moment(duration).format('D [days and] hh:mm:ss'));
         } else {
