961fec3b554ab91e56ae1624d0e94d2ccf2a89b2
Macclinson <macclinson@yahoo.com>
2015-09-10 12:03:52 +0100

    Affiliate footer links update

--- laravel/app/views/confide/forgot_password.blade.php
+++ laravel/app/views/confide/forgot_password.blade.php
@@ -28,4 +28,36 @@
     @endif
 
 </form>
+
+
+
+
+   <!-- <section class="container-fluid user-data-modal-wrapper dedicated-login-page">
+        <div class="row no-margin">
+            <div class="login-page">
+                <a href="" class="modal-box-logo clearfix">
+                    <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/logo/main-logo.png" class="img-responsive" alt="">
+                </a>
+                <div class="user-data-modal clearfix">
+                	<div class="user-data-modal-header">
+                    	<h1 class="clearfix">{{ trans('acl.enter-email-to-reset') }}</h1>
+                    </div>
+                    <div class="user-data-modal-body">
+                        <div class="form-container clearfix">
+                        <form method="POST" action="{{ action('UsersController@forgotPassword') }}" accept-charset="UTF-8" id="forgot-form" class="clearfix">
+                            <input type="hidden" name="_token" value="{{{ Session::getToken() }}}">
+                            <div class="input-append input-group">
+                                <input class="form-control no-float" placeholder="{{{ Lang::get('confide::confide.e_mail') }}}" type="text" name="email" id="email" value="{{{ Input::old('email') }}}">
+                                <span class="block text-center margin-top-20">
+                                    <input class="blue-button extra-large-button" type="submit" value="{{{ Lang::get('confide::confide.forgot.submit') }}}">
+                                </span>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>-->
+
 @stop
