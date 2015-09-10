6d03c3f769c9eb44c661ae24ce1e9f675638bedc
Arthur <arthur_yap_18@yahoo.com>
2015-09-10 17:33:37 +0800

    fixes on register page

--- laravel/app/views/confide/signup.blade.php
+++ laravel/app/views/confide/signup.blade.php
@@ -14,19 +14,17 @@
             </a>
             <div class="user-data-modal clearfix">
             	<div class="user-data-modal-header">
-                @if(isset($extraText))
+                @if(isset($extraText) && !empty($extraText))
                     <h1 class="clearfix">{{ $extraText }}</h1>
                 @else
                     <h1 class="clearfix">{{ trans('site/register.register-new-account') }}</h1>
                 @endif
-                @if($instructor_account!='affiliate')
                     <div class="login-social-buttons clearfix">
                             <a href="{{ url('login-with-facebook') }}" class="login-facebook"><span>{{ trans ('site/register.register-with-facebook') }}</span></a>
                         <!--<a href="{{url('login-with-google') }}" class="login-google">{{ trans('general.google') }}</a>-->
                     </div>
                 </div>
                     <div class="orr"><span class="left-line"></span>{{ trans('general.or') }}<span class="right-line"></span></div>
-                @endif
                 <div class="user-data-modal-body">
                     <p class="regular-paragraph text-center light-gray-text">{{ trans('site/register.enter-new-email-and-password') }}</p>
                     <div class="form-container clearfix">
