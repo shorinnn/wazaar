0fb61edb09e728c9b6c70195ff9829b69ebfd95e
Albert Maranian <albertmaranian@gmail.com>
2015-09-11 05:44:28 +0000

    Disabled video resize

--- laravel/app/views/courses/classroom/lesson-ajax.blade.php
+++ laravel/app/views/courses/classroom/lesson-ajax.blade.php
@@ -197,7 +197,7 @@
                 <div class="row video-row">
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         @if( $video != null)
-                            <video id='myVideo' controls preload="auto" style="width: 100%">
+                            <video id='myVideoxx' controls preload="auto" style="width: 100%; height: 100%">
                                 @if( Agent::isMobile() &&  $video->formats()->where('resolution', 'Low Resolution')
                                             ->first() != null)
                                     <source src="{{ $video->formats()->where('resolution', 'Low Resolution')
