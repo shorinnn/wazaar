dc6baa527256e9c78103612236271b270e78cd97
shorinnn <shorinnn@yahoo.com>
2015-09-12 00:42:42 +0300

    update for confirm passSTYLE

--- laravel/app/views/confide/confirm_password.blade.php
+++ laravel/app/views/confide/confirm_password.blade.php
@@ -1,22 +1,18 @@
-@extends('layouts.default')
-@section('content')
-    <div class="container-fluid new-dashboard top-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
-                    <h1>{{trans('general.confirm-password-to-continue')}}</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid new-dashboard dashboardTabs-header">
-        <div class="container">
-        <div class="row">
-        <div class="login-page">
-            <div class="user-data-modal clearfix">
-                <div class="user-data-modal-body">
+@extends('layouts.login_default')
+    @section('content')
+    <!--<img id="user-data-bckgrd-img" class="img-responsive" src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/background-images/background-image.jpg">
+    <span class="background-image-overlay"></span>-->
+    <section class="container-fluid user-data-modal-wrapper dedicated-login-page">
+        <div class="row no-margin">
+            <div class="login-page">
+                <a href="{{action('SiteController@index') }}" class="modal-box-logo clearfix">
+                    <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/logo/main-logo.png" class="img-responsive" alt="">
+                </a>
+                <div class="user-data-modal clearfix">
+                	<div class="user-data-modal-header">
+                    	<h1 class="clearfix">{{ trans('general.confirm-password-to-continue') }}</h1>
+                    </div>
+                    <div class="user-data-modal-body">
                         <div class="form-container clearfix">
                             @if(Session::has('error'))
                                 <p class='alert alert-danger'> {{Session::get('error')}}</p>
@@ -49,106 +45,15 @@
                     </div>
                 </div>
             </div>
-                
-            </div>
         </div>
-    </div>
+    </section>
 @stop
 
 @section('extra_js')
-    <script src="{{url('plugins/uploader/js/jquery.fileupload.js')}}"></script>
-    <script src="{{url('js/bootstrap-datetimepicker.js')}}" type="text/javascript"></script>
-    <script type="text/javascript">
+    <script>
         $(function(){
-            $('#btn-update-profile').on('click', function(){
-                var $btn = $(this);
-                $btn.button('loading');
-                var $data = $('#form-profile').serialize();
-
-
-
-                $('#errors-profile ul').html('');
-                $.post('/profile/update-profile',$data, function ($response){
-                    if ($response.success == 0){
-                        for (var $errorKey in $response.errors){
-                            $('#errors-profile ul').append('<li><i class="fa fa-exclamation"></i> '+ $response.errors[$errorKey] +'</li>');
-                        }
-
-                        $('#errors-profile').removeClass('hidden');
-                    }
-                    $btn.button('reset');
-                },'json');
-            });
-
-            $('#btn-change-password').on('click', function(){
-                var $data = $('#form-change-password').serialize();
-                var $btn = $(this);
-                $btn.button('loading');
-                $('#errors-change-password ul').html('');
-                $.post('/profile/change-password',$data, function ($response){
-                    if ($response.success == 0){
-                        for (var $errorKey in $response.errors){
-                            $('#errors-change-password ul').append('<li><i class="fa fa-exclamation"></i> '+ $response.errors[$errorKey] +'</li>');
-                        }
-
-                        $('#errors-change-password').removeClass('hidden');
-
-                    }
-                    $btn.button('reset');
-                },'json');
-            });
-
-            $('#btn-update-bank').on('click', function(){
-                var $data = $('#form-update-bank').serialize();
-                var $btn = $(this);
-                $btn.button('loading');
-                $('#errors-update-bank ul').html('');
-                $.post('/profile/update-bank-details',$data, function ($response){
-                    if ($response.success == 0){
-                        for (var $errorKey in $response.errors){
-                            $('#errors-update-bank ul').append('<li><i class="fa fa-exclamation"></i> '+ $response.errors[$errorKey] +'</li>');
-                        }
-
-                        $('#errors-update-bank').removeClass('hidden');
-
-                    }
-
-                    $btn.button('reset');
-                },'json');
-            });
-
-            $('#btn-update-info').on('click', function(){
-                var $data = $('#form-update-info').serialize();
-                var $btn = $(this);
-                $btn.button('loading');
-                $('#errors-update-info ul').html('');
-                $.post('/profile/update-other-info',$data, function ($response){
-                    if ($response.success == 0){
-                        for (var $errorKey in $response.errors){
-                            $('#errors-update-info ul').append('<li><i class="fa fa-exclamation"></i> '+ $response.errors[$errorKey] +'</li>');
-                        }
-
-                        $('#errors-update-info').removeClass('hidden');
-
-                    }
-
-                    $btn.button('reset');
-                },'json');
-            });
-
-            $('#upload-new-photo').fileupload()
-                    .bind('fileuploadprogress', function ($e, $data){
-                        var $progress = parseInt($data.loaded / $data.total * 100, 10);
-                        console.log($progress);
-                    })
-                    .bind('fileuploaddone',function ($e,$data){
-                        if ($data.result.success == 1){
-                            $('#img-profile-picture').attr('src',$data.result.photo_url);
-                        }
-            });
-
-            $('input[name=zip]').mask('999-9999');
-
+                $('[data-target="#loginModal"]').click();
         });
     </script>
 @stop
+                            
