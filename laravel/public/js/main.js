2f015fcf5fc54124abbd587f73f38de9e2050993
shorinnn <shorinnn@yahoo.com>
2015-09-14 22:54:18 +0300

    some course desc debug

--- laravel/public/js/main.js
+++ laravel/public/js/main.js
@@ -1356,7 +1356,8 @@ function showMoreContent(){
 		console.log($link);
 		console.log($content);
 	
-		var visibleHeight = $content[0].clientHeight;
+//		var visibleHeight = $content[0].clientHeight;
+		var visibleHeight = $content.height();
 		var actualHide = $content[0].scrollHeight - 1; // -1 is needed in this case or you get a 1-line offset.
 		
 		$content.css('height', visibleHeight);
