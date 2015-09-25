704994f1e751392c8c4fec4f3fa3ee40bfdb7a11
Macclinson <macclinson@yahoo.com>
2015-09-25 11:11:11 +0100

    Student dashboard style fix

--- laravel/app/views/student/dashboard/wishlist-course.blade.php
+++ laravel/app/views/student/dashboard/wishlist-course.blade.php
@@ -16,11 +16,11 @@
                                 class="img-responsive" />
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-8 col-md-7 col-lg-7">
+                    <div class="col-xs-12 col-sm-6 col-md-7 col-lg-7">
                         <h4><a href="{{ action('CoursesController@show', $course->slug) }}">{{ $course->name }}</a></h4>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-2 col-lg-2">
-                        <div class="enrolled-lessons-progress">
+                    <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 no-padding">
+                        <div class="enrolled-lessons-progress clearfix">
                             <a href="{{ action('CoursesController@show', $course->slug) }}">View</a>                                                  
                         </div>
                     </div>
