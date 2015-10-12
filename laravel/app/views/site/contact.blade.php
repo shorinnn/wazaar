53ef2450d873f5814f77cf828f7fdf4095a7cd4b
shorinnn <shorinnn@yahoo.com>
2015-10-12 22:42:55 +0300

    contact localization

--- laravel/app/views/site/contact.blade.php
+++ laravel/app/views/site/contact.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <section class="container-fluid contact-us header">
 	<div class="container">
-    	<h2>Contact Us</h2>
+    	<h2>{{ trans('site/contact.contact-us')}}</h2>
     </div>
 </section>
 
@@ -11,24 +11,23 @@
 	<div class="container">
                 <div class="row input-row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                        <label>Type of question</label>
+                        <label>{{ trans('site/contact.type-of-question')}}</label>
                     </div>
                     <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
                         <div>
-                            <input type='radio' name='user' value='Student' id='user-1' required /> Student
-                            <input type='radio' name='user' value='Instructor' id='user-2' /> Instructor
-                            <input type='radio' name='user' value='Affiliate' id='user-3' /> Affiliate
-                            <input type='radio' name='user' value='Other' id='user-4' /> Other
+                            <input type='radio' name='user' value='{{ trans('site/contact.student')}}' id='user-1' required /> {{ trans('site/contact.student')}}
+                            <input type='radio' name='user' value='{{ trans('site/contact.instructor')}}' id='user-2' /> {{ trans('site/contact.instructor')}}
+                            <input type='radio' name='user' value='{{ trans('site/contact.affiliate')}}' id='user-3' /> {{ trans('site/contact.affiliate')}}
+                            <input type='radio' name='user' value='{{ trans('site/contact.other')}}' id='user-4' /> {{ trans('site/contact.other')}}
                         </div>
                     </div>
                 </div>
                 <div class="row input-row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                        <label>Type of issue</label>
+                        <label>{{ trans('site/contact.type-of-issue')}}</label>
                     </div>
                     <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
                         <select name='question_type' required data-parsley-error-message="この値は必須です。" data-parsley-minlength="6">
-                                <option value=''>Question</option>
                                 <option value="ご質問内容をお選びください。">ご質問内容をお選びください。</option>
                                 <option value="Wazaarの使い方">Wazaarの使い方</option>
                                 <option value="会員登録">会員登録</option>
@@ -38,44 +37,43 @@
                                 <option value="その他">その他</option>
                                 <option value="Wazaarへのリクエストなど">Wazaarへのリクエストなど</option>
                         </select>
-                        <em class="block tip">Description about types, which to choose.</em>
                     </div>
                 </div>
                     <div class="row input-row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <label>Your name</label>
+                    <label>{{ trans('site/contact.your-name')}}</label>
                 </div>
                 <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
-                    <input type="text" name='name' placeholder="Enter name..." required />
+                    <input type="text" name='name' placeholder="{{ trans('site/contact.enter-name') }}" required />
                 </div>
             </div>
                     <div class="row input-row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <label>Email</label>
+                    <label>{{ trans('site/contact.email')}}</label>
 
                 </div>
                 <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
-                    <input type="email" name='email' placeholder="Enter email..." required data-parsley-trigger="change" />
-
-                </div>
-            </div>
-                    <div class="row input-row">
-                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <label>Subject</label>
-
-                </div>
-                <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
-                    <input type="text" name='subject' placeholder="Enter subject..." required />
+                    <input type="email" name='email' placeholder="Email" required data-parsley-trigger="change" />
 
                 </div>
             </div>
                     <div class="row input-row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <label>Message</label>
+                    <label>{{ trans('site/contact.subject')}}</label>
 
                 </div>
                 <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
-                    <textarea name='message' placeholder="Enter message..." required></textarea>
+                    <input type="text" name='subject' placeholder="{{ trans('site/contact.enter-subject') }}" required />
+
+                </div>
+            </div>
+                    <div class="row input-row">
+                    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <label>{{ trans('site/contact.message')}}</label>
+
+                </div>
+                <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
+                    <textarea name='message' placeholder="{{ trans('site/contact.enter-message') }}" required></textarea>
                 </div>
             </div>
                     <div class="row input-row">
@@ -83,7 +81,7 @@
 
                 </div>
                 <div class="col-xs-12 col-sm-7 col-md-4 col-lg-4">
-                    <button type='submit' class="large-button blue-button send">Send</button>
+                    <button type='submit' class="large-button blue-button send">{{ trans('site/contact.submit')}}</button>
                 </div>
             </div>
         </div>
