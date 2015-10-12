ca6c3542b1924a603187cf79134fe5666c90c54b
Arthur <arthur_yap_18@yahoo.com>
2015-10-12 23:38:07 +0800

    added padding top on no results

--- laravel/app/views/courses/categories/courses.blade.php
+++ laravel/app/views/courses/categories/courses.blade.php
@@ -5,28 +5,29 @@
 	    @endforeach
 	    <div class="clearfix"></div>
 	@else
-
-		@if(isset($search) && !empty($search))
-			<div class="text-center light-gray-text">
-				{{ trans('courses/general.no-search-result')}}
-			</div>
-		@else
-			@if(isset($subcategory->name) && !empty($subcategory->name))
-				<div class="text-center">
-					<div class="light-gray-text">{{ trans('courses/general.no-course', ['cat' => $subcategory->name] )}}</div>
-					<a class="blue-link" href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $subcategory->name] )}}</a>
-					<div class="teach-img-container">
-	                	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" />
-	                </div>
+		<div class="no-results-container">
+			@if(isset($search) && !empty($search))
+				<div class="text-center light-gray-text">
+					{{ trans('courses/general.no-search-result')}}
 				</div>
 			@else
-				<div class="text-center">
-					{{ trans('courses/general.no-course', ['cat' => $category->name] )}}<br />
-					<a href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $category->name] )}}</a><br /><br />
-	                <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" style="margin:0px auto;" />
-				</div>
-			@endif		
-		@endif
+				@if(isset($subcategory->name) && !empty($subcategory->name))
+					<div class="text-center">
+						<div class="light-gray-text">{{ trans('courses/general.no-course', ['cat' => $subcategory->name] )}}</div>
+						<a class="blue-link" href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $subcategory->name] )}}</a>
+						<div class="teach-img-container">
+		                	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" />
+		                </div>
+					</div>
+				@else
+					<div class="text-center">
+						{{ trans('courses/general.no-course', ['cat' => $category->name] )}}<br />
+						<a href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $category->name] )}}</a><br /><br />
+		                <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" style="margin:0px auto;" />
+					</div>
+				@endif		
+			@endif
+		</div>
 	@endif
 </div>  
     <!-- @if($courses->count() % 3!=0)
