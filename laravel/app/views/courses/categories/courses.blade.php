6329c711aca17735cb381c881fe21e7187e9f5a5
Arthur <arthur_yap_18@yahoo.com>
2015-10-12 23:26:46 +0800

    changes for no results found

--- laravel/app/views/courses/categories/courses.blade.php
+++ laravel/app/views/courses/categories/courses.blade.php
@@ -7,15 +7,17 @@
 	@else
 
 		@if(isset($search) && !empty($search))
-			<div class="text-center">
+			<div class="text-center light-gray-text">
 				{{ trans('courses/general.no-search-result')}}
 			</div>
 		@else
 			@if(isset($subcategory->name) && !empty($subcategory->name))
 				<div class="text-center">
-					{{ trans('courses/general.no-course', ['cat' => $subcategory->name] )}}<br />
-					<a href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $subcategory->name] )}}</a><br /><br />
-	                <img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" style="margin:0px auto;" />
+					<div class="light-gray-text">{{ trans('courses/general.no-course', ['cat' => $subcategory->name] )}}</div>
+					<a class="blue-link" href="{{action('CoursesController@create')}}"> {{ trans('courses/general.teach-new-course', ['cat' => $subcategory->name] )}}</a>
+					<div class="teach-img-container">
+	                	<img src="https://s3-ap-northeast-1.amazonaws.com/wazaar/assets/images/misc-images/teach_small.jpg" class="img-responsive" />
+	                </div>
 				</div>
 			@else
 				<div class="text-center">
