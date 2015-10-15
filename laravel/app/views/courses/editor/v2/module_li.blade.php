4fd44009257f85e10bcdbf2710db7f8eef405ca8
shorinnn <shorinnn@yahoo.com>
2015-10-15 12:20:47 +0300

    default title in curriculum sidebar https://trello.com/c/IPDcB4N4/783-course-editor-crisis

--- laravel/app/views/courses/editor/v2/module_li.blade.php
+++ laravel/app/views/courses/editor/v2/module_li.blade.php
@@ -1,6 +1,11 @@
 <li class="shr-editor-module-{{$module->id}}">
-    <a class='shr-editor-module-{{$module->id}}-copy-name scroll-to-element' href="#" data-target='div.shr-editor-module-{{$module->id}}'>
-        {{ $module->name }}</a>
+    <a  class='shr-editor-module-{{$module->id}}-copy-name scroll-to-element' href="#" data-target='div.shr-editor-module-{{$module->id}}'>
+        @if(trim($module->name)!='') 
+            {{ $module->name }}
+        @else
+            {{trans('courses/create.enter-module-name')}}
+        @endif
+    </a>
     <ol class='drag-lesson' data-module-id='{{$module->id}}'>
         @foreach($module->lessons()->orderBy('order','asc')->get() as $lesson)
             {{ View::make('courses.editor.v2.lesson_li')->with( compact('lesson') ) }}
