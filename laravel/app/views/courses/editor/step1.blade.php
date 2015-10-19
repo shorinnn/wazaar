1def9fdea305733aab984dd448591af5fd208d97
shorinnn <shorinnn@yahoo.com>
2015-10-19 14:39:11 +0300

    css fix

--- laravel/app/views/courses/editor/step1.blade.php
+++ laravel/app/views/courses/editor/step1.blade.php
@@ -27,7 +27,7 @@
                                     <div class="file-processing-handler">
                                     <p class="label-progress-bar label-progress-bar-preview-img-s1"
                                        style="position: absolute;top: -94px;color: white;width: 100%;"></p>
-                                    <div class="progress hidden">
+                                    <div class="progress" style="display:none; position: absolute;top: -74px;color: white;width: 100%; left: 10px;">
                                         <div class="progress-bar progress-bar-striped active progress-bar-preview" role="progressbar" aria-valuenow="0" 
                                              data-label=".label-progress-bar-preview-img-s1" aria-valuemin="0" aria-valuemax="100" style="width: 0%;">
                                             <span></span>
