d52aa7b940ec51e39eb870a68a8d4e73ef17e59c
Macclinson <macclinson@yahoo.com>
2015-10-23 12:17:08 +0100

    Splash page

---
+++ laravel/app/views/TEMPORARYVIEWS/splash_2.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="GWDwOY6lIKjc0pV3Pk3uK9OlW8C-qsmfdHG8sziokhw" />
+	<title>Wazaar</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <html class="ie8">
+    <![endif]-->
+
+
+    <style>
+        .splash_2_header{
+            width: 100%;
+            padding: 12px;
+            background: #18222b;
+            text-align: center;
+        }
+        .splash_2_header > div{
+            display: inline-block;
+            max-width: 150px;
+        }
+        .splash_2 p{
+            font-size: 26px;
+            color: #303941;
+        }
+        .splash_2 .para-holder{
+            margin: 40px 0;
+        }
+        .splash_2 .splash-img-wrap{
+            display: inline-block;
+            margin-top: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="splash_2">
+    <div class="splash_2_header">
+        <div>
+            <img src="splash/logo.png" class="img-responsive">
+        </div>
+    </div>
+    <section class="text-center para-holder">
+        <p>サービス向上に向け、</p>
+        <p>2016年春リニューアルオープン！</p>
+        <div class="splash-img-wrap">
+            <img src="splash/splashimg_small.png" class="img-responsive">
+        </div>
+    </section>
+</div>
+</body>
+</html>
